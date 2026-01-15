2b1c26bd93a556db484aec944c6e2a984c3e170b
SP9BSK <ratownikxl@gmail.com>
2026-01-15 19:05:31 +0100

    Add MainMenuActivity class for main menu

---
+++ app/app/src/main/app/src/main/java/com/example/quizpozarniczy/MainMenuActivity.kt
@@ -0,0 +1,10 @@
+package com.example.quizpozarniczy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
