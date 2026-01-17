e719f8b9b184a800682fedc3cfba96682d53f0b0
SP9BSK <ratownikxl@gmail.com>
2026-01-17 18:12:10 +0100

    Update QuizRepository.kt

--- app/src/main/java/com/example/quizpozarniczy/QuizRepository.kt
+++ app/src/main/java/com/example/quizpozarniczy/QuizRepository.kt
@@ -1,11 +1,5 @@
 package com.example.quizpozarniczy
 
-data class Question(
-    val question: String,
-    val answers: List<String>,
-    val correct: Int
-)
-
 object QuizRepository {
 
     fun getQuestions(): List<Question> {
@@ -28,11 +22,6 @@ object QuizRepository {
             Question(
                 "Jaki gaz jest niezbędny do spalania?",
                 listOf("Azot", "Tlen", "Dwutlenek węgla", "Hel"),
-                1
-            ),
-            Question(
-                "Jakim kolorem oznacza się hydrant?",
-                listOf("Niebieskim", "Czerwonym", "Zielonym", "Żółtym"),
                 1
             )
         )
