57dc01e585f109a1a63121c3d5aa99072ae64539
SP9BSK <ratownikxl@gmail.com>
2026-01-18 07:24:06 +0100

    Update QuizRepository.kt

--- app/src/main/java/com/example/quizpozarniczy/QuizRepository.kt
+++ app/src/main/java/com/example/quizpozarniczy/QuizRepository.kt
@@ -1,1 +1,29 @@
+package com.example.quizpozarniczy
 
+object QuizRepository {
+
+    fun getQuestions(): List<Question> {
+        return listOf(
+            Question(
+                "Jaki jest numer alarmowy straży pożarnej?",
+                listOf("997", "998", "112", "999"),
+                1
+            ),
+            Question(
+                "Co oznacza skrót PSP?",
+                listOf(
+                    "Państwowa Straż Pożarna",
+                    "Publiczna Służba Pożarna",
+                    "Państwowa Służba Porządkowa",
+                    "Polska Straż Pożarna"
+                ),
+                0
+            ),
+            Question(
+                "Jakim kolorem oznacza się hydrant?",
+                listOf("Niebieski", "Zielony", "Czerwony", "Żółty"),
+                2
+            )
+        )
+    }
+}
