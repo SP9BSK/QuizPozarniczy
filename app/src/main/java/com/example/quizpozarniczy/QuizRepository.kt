4519716ebb2dd940bbb412b53263fbd74e6e4e96
SP9BSK <ratownikxl@gmail.com>
2026-01-18 16:06:37 +0100

    Update QuizRepository.kt

--- app/src/main/java/com/example/quizpozarniczy/QuizRepository.kt
+++ app/src/main/java/com/example/quizpozarniczy/QuizRepository.kt
@@ -4,26 +4,11 @@ object QuizRepository {
 
     fun getQuestions(): List<Question> {
         return listOf(
-            Question(
-                "Jaki jest numer alarmowy straży pożarnej?",
-                listOf("997", "998", "112", "999"),
-                1
-            ),
-            Question(
-                "Co oznacza skrót PSP?",
-                listOf(
-                    "Państwowa Straż Pożarna",
-                    "Publiczna Służba Pożarna",
-                    "Państwowa Służba Porządkowa",
-                    "Polska Straż Pożarna"
-                ),
-                0
-            ),
-            Question(
-                "Jakim kolorem oznacza się hydrant?",
-                listOf("Niebieski", "Zielony", "Czerwony", "Żółty"),
-                2
-            )
+            Question("Czy strażak może pracować bez hełmu?", false),
+            Question("Czy numer alarmowy to 112?", true),
+            Question("Czy pożarów nie gasi się wodą?", false),
+            Question("Czy OSP jest częścią KSRG?", true),
+            Question("Czy aparat ODO służy do oddychania?", true)
         )
     }
 }
