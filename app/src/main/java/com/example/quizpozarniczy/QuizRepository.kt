d5e938d1fb12c1b9c22a4e51feed86f5810b9e77
SP9BSK <ratownikxl@gmail.com>
2026-01-18 07:23:28 +0100

    Update QuizRepository.kt

--- app/src/main/java/com/example/quizpozarniczy/QuizRepository.kt
+++ app/src/main/java/com/example/quizpozarniczy/QuizRepository.kt
@@ -1,14 +1,1 @@
-package com.example.quizpozarniczy
 
-object QuizRepository {
-
-    fun getQuestions(): List<Question> {
-        return listOf(
-            Question("Jaki jest numer alarmowy straży pożarnej?"),
-            Question("Co oznacza skrót PSP?"),
-            Question("Jakim kolorem oznacza się hydrant?"),
-            Question("Co robić jako pierwsze przy pożarze?"),
-            Question("Kto dowodzi akcją ratowniczą?")
-        )
-    }
-}
