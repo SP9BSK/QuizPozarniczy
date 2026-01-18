e5ae43f6ed8cf755c53fc874289438619b1f2efc
SP9BSK <ratownikxl@gmail.com>
2026-01-18 07:26:05 +0100

    Update ResultActivity.kt

--- app/src/main/java/com/example/quizpozarniczy/ResultActivity.kt
+++ app/src/main/java/com/example/quizpozarniczy/ResultActivity.kt
@@ -1,8 +1,6 @@
 package com.example.quizpozarniczy
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,19 +10,10 @@ class ResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
-        val score = intent.getIntExtra("score", 0)
-        val max = intent.getIntExtra("max", 0)
+        val score = intent.getIntExtra("SCORE", 0)
+        val total = intent.getIntExtra("TOTAL", 0)
 
-        val txtResult = findViewById<TextView>(R.id.txtResult)
-        val btnBack = findViewById<Button>(R.id.btnBackToJudge)
-
-        txtResult.text = "Wynik: $score / $max"
-
-        btnBack.setOnClickListener {
-            val intent = Intent(this, JudgeActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
-            startActivity(intent)
-            finish()
-        }
+        findViewById<TextView>(R.id.txtResult).text =
+            "Wynik: $score / $total"
     }
 }
