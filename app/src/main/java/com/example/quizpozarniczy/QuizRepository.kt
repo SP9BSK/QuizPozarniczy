6c363d427219334452d412d093aa36476fbae6de
SP9BSK <ratownikxl@gmail.com>
2026-01-20 15:58:55 +0100

    Update QuizRepository.kt

--- app/src/main/java/com/example/quizpozarniczy/QuizRepository.kt
+++ app/src/main/java/com/example/quizpozarniczy/QuizRepository.kt
@@ -7,19 +7,64 @@ object QuizRepository {
     fun getQuestions(): List<Question> {
         return listOf(
             Question(
-                "Co jest pierwszym środkiem gaśniczym?",
-                listOf("Woda", "Piana", "Piasek", "Proszek"),
+                "Numer alarmowy do straży pożarnej to:",
+                listOf("998", "997", "999", "112"),
                 0
             ),
             Question(
-                "Numer alarmowy straży pożarnej to:",
-                listOf("997", "998", "999", "112"),
+                "Kto dowodzi akcją ratowniczą?",
+                listOf(
+                    "Najstarszy strażak",
+                    "Dowódca zastępu",
+                    "Dyżurny PSP",
+                    "Kierowca"
+                ),
                 1
             ),
             Question(
-                "Kolor hełmu dowódcy to:",
-                listOf("Czarny", "Biały", "Czerwony", "Żółty"),
+                "Kolor hełmu dowódcy OSP to:",
+                listOf("Czerwony", "Biały", "Czarny", "Żółty"),
                 1
+            ),
+            Question(
+                "Co oznacza skrót PSP?",
+                listOf(
+                    "Państwowa Straż Pożarna",
+                    "Polska Straż Pożarna",
+                    "Publiczna Straż Pożarna",
+                    "Pożarna Służba Państwowa"
+                ),
+                0
+            ),
+            Question(
+                "Jakim środkiem gasi się pożary grupy F?",
+                listOf("Woda", "Piana", "Proszek", "Tłuszcz"),
+                1
+            ),
+            Question(
+                "Kto może ogłosić alarm OSP?",
+                listOf(
+                    "Każdy strażak",
+                    "Sołtys",
+                    "Dyżurny SKKP",
+                    "Dowódca JRG"
+                ),
+                2
+            ),
+            Question(
+                "Co oznacza skrót OSP?",
+                listOf(
+                    "Ochotnicza Straż Pożarna",
+                    "Obronna Straż Publiczna",
+                    "Organizacja Straży Pożarnej",
+                    "Ochronna Służba Pożaru"
+                ),
+                0
+            ),
+            Question(
+                "Podstawowy środek gaśniczy w PSP to:",
+                listOf("Woda", "Piana", "Proszek", "CO2"),
+                0
             )
         )
     }
