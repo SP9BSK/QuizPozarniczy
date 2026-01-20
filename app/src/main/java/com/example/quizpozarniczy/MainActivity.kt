7fdce11ea748a6610fb93897050aadacb6ee7aaf
SP9BSK <ratownikxl@gmail.com>
2026-01-20 11:05:26 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/quizpozarniczy/MainActivity.kt
+++ app/src/main/java/com/example/quizpozarniczy/MainActivity.kt
@@ -2,17 +2,17 @@ package com.example.quizpozarniczy
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun openJudge(v: View) {
-        startActivity(Intent(this, JudgeActivity::class.java))
+        findViewById<Button>(R.id.btnJudge).setOnClickListener {
+            startActivity(Intent(this, JudgeActivity::class.java))
+        }
     }
 }
