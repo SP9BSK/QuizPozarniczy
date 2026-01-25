c52a8bc0c0b96002f9a3db148b5a6fbf76cfaff7
SP9BSK <ratownikxl@gmail.com>
2026-01-25 11:32:47 +0100

    Update StartActivity.kt

--- app/src/main/java/com/example/quizpozarniczy/StartActivity.kt
+++ app/src/main/java/com/example/quizpozarniczy/StartActivity.kt
@@ -6,10 +6,15 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class StartActivity : AppCompatActivity() {
+window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+override fun onPause() {
+    super.onPause()
+    window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+}
 
         val btnJudge = findViewById<Button>(R.id.btnJudge)
 
