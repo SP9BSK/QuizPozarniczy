aa6b2f352ffb2961d042c8ec20e28a3411a852d8
SP9BSK <ratownikxl@gmail.com>
2026-01-26 18:54:41 +0100

    Update QuestionsPart2.kt

--- app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart2.kt
+++ app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart2.kt
@@ -5,38 +5,141 @@ import com.example.quizpozarniczy.model.Question
 val questionsPart2 = listOf(
 
     Question(
-        text = "Jaka jest praktyczna wysokość ssania motopomp strażackich?",
-        answers = listOf("7,5 m", "7 m", "6,5 m"),
-        correctIndex = 0
-    ),
-
-    Question(
-        text = "Drabina dwuprzęsłowa wysuwana może być sprawiona jako:",
-        answers = listOf("Przyparta", "Odchylona", "Wolnostojąca"),
-        correctIndex = 2
-    ),
-
-    Question(
-        text = "Jaka jest normatywna szerokość poziomych dróg ewakuacyjnych w nowo projektowanych budynkach?",
-        answers = listOf("1,5 m", "1,4 m", "1,8 m"),
-        correctIndex = 1
-    ),
-
-    Question(
-        text = "Związek Ochotniczych Straży Pożarnych został powołany w roku:",
-        answers = listOf("1948", "1958", "1956"),
-        correctIndex = 2
-    ),
-
-    Question(
-        text = "Co stanie się z płomieniem świecy, jeżeli w pomieszczeniu zwiększymy stężenie tlenu?",
-        answers = listOf(
-            "Płomień wydłuży się",
-            "Płomień skróci się i rozszerzy",
-            "Płomień zgaśnie"
+            text = "Jaka jest praktyczna wysokość ssania motopomp strażackich?",
+            answers = listOf("7,5 m", "7 m", "6,5 m"),
+            correctIndex = 0
         ),
-        correctIndex = 0
-    )
 
-    // kolejne pytania…
-)
+        Question(
+            text = "Drabina dwuprzęsłowa wysuwana może być sprawiona jako:",
+            answers = listOf("przyparta", "odchylona", "wolnostojąca"),
+            correctIndex = 2
+        ),
+
+        Question(
+            text = "Jaka jest normatywna szerokość poziomych dróg ewakuacyjnych w nowo projektowanych budynkach?",
+            answers = listOf("1,5 m", "1,4 m", "1,8 m"),
+            correctIndex = 1
+        ),
+
+        Question(
+            text = "Związek Ochotniczych Straży Pożarnych został powołany w roku:",
+            answers = listOf("1948", "1958", "1956"),
+            correctIndex = 2
+        ),
+
+        Question(
+            text = "Co stanie się z płomieniem świecy, jeżeli w pomieszczeniu zwiększymy stężenie tlenu?",
+            answers = listOf(
+                "płomień wydłuży się",
+                "płomień skróci się i rozszerzy",
+                "płomień zgaśnie"
+            ),
+            correctIndex = 0
+        ),
+
+        Question(
+            text = "Muzea, jako budynki można zaliczyć do kategorii zagrożenia ludzi:",
+            answers = listOf("ZL I", "ZL IV", "ZL V"),
+            correctIndex = 2
+        ),
+
+        Question(
+            text = "Co oznacza symbol GBAM?",
+            answers = listOf(
+                "samochód specjalny gaśniczy z autopompą",
+                "samochód specjalny gaśniczy ze zbiornikiem autopompą i motopompą",
+                "samochód gaśniczy ze zbiornikiem z autopompą i motopompą"
+            ),
+            correctIndex = 2
+        ),
+
+        Question(
+            text = "W którym roku powołano Związek Ochotniczych Straży Pożarnych RP?",
+            answers = listOf("1975", "1995", "1992"),
+            correctIndex = 2
+        ),
+
+        Question(
+            text = "Wokół placów składowych należy zachować pas ochronny o szerokości minimum:",
+            answers = listOf("1 m", "2 m", "4 m"),
+            correctIndex = 1
+        ),
+
+        Question(
+            text = "Z przewodów dymowych i spalinowych w budynkach opalanych paliwem stałym należy usuwać zanieczyszczenia:",
+            answers = listOf(
+                "co najmniej 2 razy w roku",
+                "co najmniej 4 razy w roku",
+                "co najmniej raz do roku"
+            ),
+            correctIndex = 1
+        ),
+
+        Question(
+            text = "Budynki niskie to:",
+            answers = listOf(
+                "budynki mieszkalne o wysokości do 4 kondygnacji włącznie",
+                "budynki mieszkalne o wysokości do 9 kondygnacji włącznie",
+                "budynki mieszkalne o wysokości do 2 kondygnacji włącznie"
+            ),
+            correctIndex = 0
+        ),
+
+        Question(
+            text = "Jaki jest najwyższy stopień zagrożenia pożarowego lasu?",
+            answers = listOf("I", "III", "II"),
+            correctIndex = 1
+        ),
+
+        Question(
+            text = "Co to jest wstrząs pourazowy?",
+            answers = listOf(
+                "reakcja organizmu na uraz",
+                "uderzenie po upadku",
+                "utrata przytomności"
+            ),
+            correctIndex = 0
+        ),
+
+        Question(
+            text = "Jednostką organizacyjną najniższego szczebla w PSP jest:",
+            answers = listOf(
+                "oddział ratowniczy",
+                "zawodowa straż pożarna",
+                "jednostka ratowniczo-gaśnicza"
+            ),
+            correctIndex = 2
+        ),
+
+        Question(
+            text = "Minimum ile osób potrzeba do utworzenia OSP?",
+            answers = listOf("10", "15", "20"),
+            correctIndex = 1
+        ),
+
+        Question(
+            text = "Elew to słuchacz szkoły:",
+            answers = listOf("podoficerskiej", "aspirantów", "oficerskiej"),
+            correctIndex = 0
+        ),
+
+        Question(
+            text = "Ile osób liczy korpus oficerów w Straży Pożarnej?",
+            answers = listOf("8", "5", "6"),
+            correctIndex = 0
+        ),
+
+        Question(
+            text = "Patron strażaków trzyma w ręku:",
+            answers = listOf("wiadro", "toporek", "bosak"),
+            correctIndex = 0
+        ),
+
+        Question(
+            text = "Do sprzętu gaśniczego powinien być zapewniony dostęp co najmniej:",
+            answers = listOf("1 m", "1,5 m", "1,6 m"),
+            correctIndex = 0
+        )
+    )
+}
