d85a08ceb2ab9ce81a357eaa62e7eab8ad5cbf9a
SP9BSK <ratownikxl@gmail.com>
2026-01-26 09:04:38 +0100

    Update StartActivity.kt

--- app/src/main/java/com/example/quizpozarniczy/StartActivity.kt
+++ app/src/main/java/com/example/quizpozarniczy/StartActivity.kt
@@ -2,16 +2,24 @@ package com.example.quizpozarniczy
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
-class StartActivity : BaseActivity() {
+class StartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
 
-        findViewById<Button>(R.id.btnStart).setOnClickListener {
-            startActivity(Intent(this, JudgeActivity::class.java))
+        // 🔒 ekran zawsze włączony
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        val btnJudge = findViewById<Button>(R.id.btnJudge)
+
+        btnJudge.setOnClickListener {
+            val intent = Intent(this, JudgeActivity::class.java)
+            startActivity(intent)
         }
     }
 }
