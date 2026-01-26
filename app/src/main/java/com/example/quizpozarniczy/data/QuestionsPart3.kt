926cbc16aed9fb8e18f5ebc6e95f0dc117523f16
SP9BSK <ratownikxl@gmail.com>
2026-01-26 19:10:03 +0100

    Create QuestionsPart3.kt

---
+++ app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart3.kt
@@ -0,0 +1,166 @@
+package com.example.quizpozarniczy.data
+
+import com.example.quizpozarniczy.model.Question
+
+val questionsPart3 = listOf(
+
+    Question(
+        text = "Szkoła kształcąca kadry oficerskie Państwowej Straży Pożarnej znajduje się w Warszawie i nosi nazwę:",
+        answers = listOf(
+            "Centralna Szkoła Pożarnicza",
+            "Wyższa Oficerska Szkoła Pożarnicza",
+            "Akademia Pożarnicza"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Jaka jest normatywna szerokość poziomych dróg ewakuacyjnych w nowo projektowanych budynkach, jeżeli są one przeznaczone do ewakuacji dla nie więcej niż 20 osób?",
+        answers = listOf("1,2 m", "1,4 m", "0,9 m"),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "W Warszawie w „Domu Strażaka” przy ul. Oboźnej posiada swoją siedzibę:",
+        answers = listOf(
+            "Centrum Zarządzania Kryzysowego Państwa",
+            "Zarząd Główny ZOSP RP",
+            "Komenda Główna PSP"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "W jakiej odległości od skraju drogi publicznej zabronione jest pozostawienie gałęzi, chrustu itp.?",
+        answers = listOf("Do 50 m", "Do 25 m", "75 m"),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Środki zwilżające są to środki, które:",
+        answers = listOf(
+            "Zmniejszają opory tłoczenia wody",
+            "Zwiększają napięcie powierzchniowe wody",
+            "Obniżają napięcie powierzchniowe wody"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Pożary butli z acetylenem to pożary grupy:",
+        answers = listOf("C", "D", "B"),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Środkiem uodparniającym drewno na działanie ognia jest:",
+        answers = listOf("Farba olejna", "Pyran", "Fobos"),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Sterty i stogi można ustawiać od budynków palnych w odległości co najmniej:",
+        answers = listOf("20 m", "30 m", "50 m"),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Obowiązujące miano określające gęstość obciążenia ogniowego to:",
+        answers = listOf("kg/m²", "MJ/m³", "kcal/kg"),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Przy poborze wody z otwartego zbiornika smok ssawny powinien być zanurzony:",
+        answers = listOf(
+            "15–20 cm od dna zbiornika",
+            "20–40 cm poniżej lustra wody",
+            "15–20 cm poniżej lustra wody"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Jaka jest minimalna pojemność przeciwpożarowego zbiornika wodnego?",
+        answers = listOf("50 m³", "75 m³", "100 m³"),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Które z wymienionych substancji można gasić wodą?",
+        answers = listOf("Sód", "Magnez", "Słoma"),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Zaczadzenie następuje na skutek działania:",
+        answers = listOf("CO", "CO₂", "N"),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Komendanta Głównego PSP powołuje i odwołuje:",
+        answers = listOf(
+            "Prezydent RP na wniosek Ministra Spraw Wewnętrznych i Administracji",
+            "Prezes Rady Ministrów na wniosek Ministra Spraw Wewnętrznych i Administracji",
+            "Prezes Rady Ministrów"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Ile jest stopni zagrożenia pożarowego lasu?",
+        answers = listOf("2", "4", "3"),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Gaz propan–butan jest szczególnie niebezpieczny ponieważ:",
+        answers = listOf(
+            "Jest lżejszy od powietrza",
+            "Rozchodzi się we wszystkich kierunkach",
+            "Jest cięższy od powietrza"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Prądownica pianowa PP 2–12 oznacza:",
+        answers = listOf(
+            "Ciśnienie 2 atm.; nominalna wydajność wodna 120 l/min",
+            "Liczba spienienia 2; nominalna wydajność wodna 12 l/s",
+            "Nominalna wydajność wodna 200 l/min; liczba spienienia 12"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Klapa dymowa to:",
+        answers = listOf(
+            "Urządzenie służące do gaszenia cieczy palnych w zbiornikach",
+            "Urządzenie służące do zamykania zbiorników z gazami palnymi",
+            "Pokrywa zamontowana w dachu budynku umożliwiająca usunięcie z pomieszczeń dymu i gorących gazów pożarowych"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Linia zasilająca to:",
+        answers = listOf(
+            "Linia wężowa od nasady tłocznej pompy pożarniczej do rozdzielacza",
+            "Linia wężowa od punktu czerpania wody do nasady ssawnej pompy",
+            "Linia wężowa od nasady hydrantu do nasady ssawnej pompy lub bezpośrednio do zbiornika wodnego samochodu gaśniczego"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Nomex to:",
+        answers = listOf(
+            "Środek gaśniczy",
+            "Materiał z którego wykonuje się ubrania strażackie",
+            "Środek ogniochronny"
+        ),
+        correctIndex = 1
+    )
+)
