f7d220573501fd7330d55c6d6a40a6fd6104e57e
SP9BSK <ratownikxl@gmail.com>
2026-01-26 07:35:44 +0100

    Update StartActivity.kt

--- app/src/main/java/com/example/quizpozarniczy/StartActivity.kt
+++ app/src/main/java/com/example/quizpozarniczy/StartActivity.kt
@@ -2,27 +2,16 @@ package com.example.quizpozarniczy
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.WindowManager
 import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
 
-class StartActivity : AppCompatActivity() {
+class StartActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
 
-        // ⛔ na ekranie startowym EKRAN MOŻE GASNĄĆ
-        window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-
-        val btnStart = findViewById<Button>(R.id.btnStart)
-
-        btnStart.setOnClickListener {
-            val intent = Intent(this, JudgeActivity::class.java)
-            startActivity(intent)
-
-            // 🔴 BARDZO WAŻNE
-            finish()
+        findViewById<Button>(R.id.btnStart).setOnClickListener {
+            startActivity(Intent(this, JudgeActivity::class.java))
         }
     }
 }
