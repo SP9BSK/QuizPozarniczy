8a7cce755af5bf9c092cdf927f0ec118583fcf2a
SP9BSK <ratownikxl@gmail.com>
2026-01-28 11:29:58 +0100

    Create QuestionsPart5.kt

---
+++ app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart5.kt
@@ -0,0 +1,167 @@
+package com.example.quizpozarniczy.data
+
+import com.example.quizpozarniczy.model.Question
+
+val questionsPart5 = listOf(
+
+    Question(
+        text = "Ochotnicze Straże Pożarne są:",
+        answers = listOf(
+            "stowarzyszeniami",
+            "jednostkami organizacyjnymi PSP",
+            "organami administracji rządowej"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Co ile lat w OSP odbywają się zebrania sprawozdawczo-wyborcze (zebrania statutowe)?",
+        answers = listOf(
+            "2 lata",
+            "4 lata",
+            "5 lat"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Szerokość wyjścia ewakuacyjnego nie może być mniejsza niż:",
+        answers = listOf(
+            "0,8 m",
+            "0,9 m",
+            "0,6 m"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Jaki jest najwyższy stopień aspirancki?",
+        answers = listOf(
+            "aspirant sztabowy",
+            "starszy aspirant sztabowy",
+            "starszy aspirant"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Jaki rodzaj drzewostanu jest najbardziej podatny na rozwój pożaru?",
+        answers = listOf(
+            "iglasty",
+            "liściasty",
+            "mieszany"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Środkiem uodporniającym drewno na działanie ognia jest:",
+        answers = listOf(
+            "pyran",
+            "środek zwilżający",
+            "fobos"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Symbol SH-10 oznacza:",
+        answers = listOf(
+            "samochód specjalny holowniczy o mocy 100 KM",
+            "samochód specjalny, średni, podnośnik hydrauliczny o wysięgu 10 m",
+            "samochód specjalny, podnośnik hydrauliczny o wysokości 10 m i udźwigu 10 ton"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Wycofanym środkiem gaśniczym z uwagi na ochronę środowiska jest:",
+        answers = listOf(
+            "azot",
+            "CO₂",
+            "halon"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Eksplozymetr to:",
+        answers = listOf(
+            "urządzenie pomiaru siły eksplozji",
+            "urządzenie wyłączające klapy odcinające w czasie wybuchu",
+            "urządzenie do pomiaru stężeń wybuchowych"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Czy wolno napełniać gazem butle o masie 11 kg na stacjach gazu płynnego?",
+        answers = listOf(
+            "nie",
+            "tak, jeżeli wykonana jest niezależna instalacja",
+            "tak"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Pojemność zbiornika wodnego w samochodzie GCBA 5/24 wynosi:",
+        answers = listOf(
+            "2400 l",
+            "5000 l",
+            "500 l"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Do wytwarzania i podawania piany średniej służy:",
+        answers = listOf(
+            "wytwornica pianowa",
+            "prądownica pianowa",
+            "agregat pianowy"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Tryskacze służą do:",
+        answers = listOf(
+            "szybkiego uruchomienia samochodu gaśniczego",
+            "gaszenia pożaru",
+            "wykrywania pożaru"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Jaka jest średnica nominalna hydrantów zewnętrznych podziemnych?",
+        answers = listOf(
+            "80, 100",
+            "100, 125",
+            "80"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Zaczadzenie następuje wskutek działania:",
+        answers = listOf(
+            "CO",
+            "CO₂",
+            "N₂"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "W przeciwpożarowe wyłączniki prądu muszą być wyposażone obiekty:",
+        answers = listOf(
+            "o powierzchni użytkowej ponad 1000 m²",
+            "o kubaturze powyżej 1000 m³",
+            "o wysokości powyżej 12 m"
+        ),
+        correctIndex = 1
+    )
+
+)
