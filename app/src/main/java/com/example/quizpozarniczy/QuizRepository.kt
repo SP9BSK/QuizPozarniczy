20c4bcea23aa0df001c0353ce51099d7c024dded
SP9BSK <ratownikxl@gmail.com>
2026-01-28 12:09:12 +0100

    Update QuizRepository.kt

--- app/src/main/java/com/example/quizpozarniczy/QuizRepository.kt
+++ app/src/main/java/com/example/quizpozarniczy/QuizRepository.kt
@@ -3,11 +3,16 @@ package com.example.quizpozarniczy
 import com.example.quizpozarniczy.data.questionsPart1
 import com.example.quizpozarniczy.data.questionsPart2
 import com.example.quizpozarniczy.data.questionsPart3
+import com.example.quizpozarniczy.data.questionsPart4
+import com.example.quizpozarniczy.data.questionsPart5
+import com.example.quizpozarniczy.data.questionsPart6
+import com.example.quizpozarniczy.data.questionsPart7
+import com.example.quizpozarniczy.data.questionsPart8
 import com.example.quizpozarniczy.model.Question
 
 object QuizRepository {
 
     fun getQuestions(): List<Question> {
-        return questionsPart1 + questionsPart2 + questionsPart3
+        return questionsPart1 + questionsPart2 + questionsPart3 + questionsPart4 + questionsPart5 + questionsPart6 + questionsPart7 + questionsPart8
     }
 }
