5b8fcf9e67394d6b3d111b00801121dab4e47ecd
SP9BSK <ratownikxl@gmail.com>
2026-01-28 11:41:51 +0100

    Create QuestionsPart6.kt

---
+++ app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart6.kt
@@ -0,0 +1,197 @@
+package com.example.quizpozarniczy.data
+
+import com.example.quizpozarniczy.model.Question
+
+val questionsPart6 = listOf(
+
+    Question(
+        text = "W jednostkach PSP jako podstawowy system zabezpieczenia dróg oddechowych stosuje się:",
+        answers = listOf(
+            "aparaty tlenowe",
+            "aparaty powietrzne nadciśnieniowe",
+            "aparaty powietrzne podciśnieniowe"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Flame Control to środek:",
+        answers = listOf(
+            "gaśniczy",
+            "ogniochronny",
+            "absorbent"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Prawo do wyznaczania w lesie miejsca na palenie ogniska ma:",
+        answers = listOf(
+            "właściciel lub zarządca lasu",
+            "Komendant Powiatowy lub Miejski PSP",
+            "osoba dorosła, pod warunkiem zachowania środków ostrożności"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Do gazów gaśniczych można zaliczyć:",
+        answers = listOf(
+            "azot",
+            "etan",
+            "metan"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Pianą można gasić pożary klasy:",
+        answers = listOf(
+            "klasy A i B",
+            "klasy C i D",
+            "tylko klasy C"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Pożary metali lekkich zaliczają się do klasy pożarów:",
+        answers = listOf(
+            "A",
+            "B",
+            "D"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Po haśle „Do akcji gotuj” przy zasilaniu samochodu z hydrantu, kierowca:",
+        answers = listOf(
+            "pomaga w budowie linii głównej",
+            "przeprowadza rozpoznanie wodne",
+            "ustawia samochód w miejscu wskazanym przez dowódcę, a następnie uruchamia autopompę"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Czego nie wolno gasić wodą?",
+        answers = listOf(
+            "drewna",
+            "węgla",
+            "sodu"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Jaki narząd jest najbardziej wrażliwy na niedotlenienie?",
+        answers = listOf(
+            "serce",
+            "mózg",
+            "nerki"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "W mięśniu czwórgłowym uda znajduje się wbity pręt metalowy. Pierwsza pomoc polega na:",
+        answers = listOf(
+            "szybkim usunięciu przedmiotu",
+            "zabezpieczeniu przedmiotu przed przemieszczeniem i opatrzeniu rany",
+            "zebraniu danych osobowych póki pacjent jest przytomny"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Wstrząs jest to:",
+        answers = listOf(
+            "stan niedotlenienia mózgu",
+            "zespół drgawek",
+            "odruch człowieka na działanie prądu elektrycznego"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Krew tętniczą wypływającą z rany poznamy po:",
+        answers = listOf(
+            "ciemnobrunatnej barwie",
+            "spokojnym wypływie",
+            "jasnoczerwonej barwie"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Co oznacza znak gestowy „Prawa ręka podniesiona w górę”?",
+        answers = listOf(
+            "woda stój",
+            "woda naprzód",
+            "uwaga"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Mostek przejazdowy służy do:",
+        answers = listOf(
+            "umożliwienia przejazdu samochodu przez rzekę",
+            "zabezpieczenia węży przed uszkodzeniem układanych na ulicach i drogach",
+            "sprawiania drabiny pożarniczej"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Dym biało-żółty wydziela się podczas palenia:",
+        answers = listOf(
+            "siana",
+            "celuloidu",
+            "gumy"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Stosunek służbowy strażaka powstaje z dniem:",
+        answers = listOf(
+            "powołania",
+            "mianowania",
+            "ślubowania"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Podoficerom pełniącym służbę w jednostkach ratowniczo-gaśniczych kolejne stopnie służbowe nadaje:",
+        answers = listOf(
+            "Komendant Miejski / Powiatowy PSP",
+            "Komendant Wojewódzki PSP",
+            "Komendant Główny PSP"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Wytwornica pianowa WP 2/75 służy do wytwarzania piany:",
+        answers = listOf(
+            "średniej",
+            "lekkiej",
+            "ciężkiej"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "W jakim mieście na terenie woj. kujawsko-pomorskiego znajduje się Szkoła Podoficerska?",
+        answers = listOf(
+            "Toruń",
+            "Włocławek",
+            "Bydgoszcz"
+        ),
+        correctIndex = 2
+    )
+
+)
