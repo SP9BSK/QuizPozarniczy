c3e6af3b4baa3f51ec0636c34aaa356df74f443e
SP9BSK <ratownikxl@gmail.com>
2026-01-28 11:55:31 +0100

    Create QuestionsPart7.kt

---
+++ app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart7.kt
@@ -0,0 +1,267 @@
+package com.example.quizpozarniczy.data
+
+import com.example.quizpozarniczy.model.Question
+
+val questionsPart7 = listOf(
+
+    Question(
+        text = "Obowiązująca ustawa o ochronie przeciwpożarowej została uchwalona:",
+        answers = listOf(
+            "3 lipca 1980 r.",
+            "3 listopada 1992 r.",
+            "24 sierpnia 1991 r."
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Co najmniej dwa wyjścia ewakuacyjne należy projektować z pomieszczeń, w których będzie przebywać jednocześnie ponad:",
+        answers = listOf(
+            "20 osób",
+            "50 osób",
+            "75 osób"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Biblioteki zalicza się do kategorii zagrożenia ludzi ZL:",
+        answers = listOf(
+            "ZL III",
+            "ZL V",
+            "ZL IV"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Liczba spienienia to:",
+        answers = listOf(
+            "stosunek ilości piany do ilości środka pianotwórczego, z którego ta piana została wytworzona",
+            "liczba pęcherzyków powietrza zawarta w 1 dm³ piany",
+            "stosunek ilości wodnego roztworu środka pianotwórczego do wydajności piany"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Szerokość poziomych dróg ewakuacyjnych w budynku nie może być mniejsza niż:",
+        answers = listOf(
+            "0,6 m",
+            "1,5 m",
+            "1,4 m"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Wysokość lokalnego obniżenia na drodze ewakuacyjnej w budynku nie może być mniejsza niż:",
+        answers = listOf(
+            "1,8 m",
+            "2 m",
+            "2,2 m"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "W jakim przypadku w budynku zaliczonym do ZL III należy stosować klatki schodowe obudowane i zamykane drzwiami?",
+        answers = listOf(
+            "w budynkach niskich",
+            "we wszystkich budynkach",
+            "w budynkach średniowysokich"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Dojście ewakuacyjne to:",
+        answers = listOf(
+            "długość drogi ewakuacyjnej od wyjścia z pomieszczenia na tę drogę do wyjścia do innej strefy pożarowej lub na zewnątrz budynku, mierzoną wzdłuż osi drogi ewakuacyjnej",
+            "odległość od najdalszego miejsca w pomieszczeniu do wyjścia na drogę ewakuacyjną",
+            "odległość od najdalszego miejsca w pomieszczeniu do wyjścia na zewnątrz budynku"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Stosowanie łatwo zapalnych wykładzin podłogowych jest zabronione:",
+        answers = listOf(
+            "w pomieszczeniach, w których może przebywać więcej niż 50 osób",
+            "w pomieszczeniach okratowanych",
+            "w budynkach zaliczonych do ZL III"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Dopuszczalna długość dojść ewakuacyjnych przy jednym dojściu w strefie pożarowej zagrożonej wybuchem wynosi:",
+        answers = listOf(
+            "15 m",
+            "10 m",
+            "25 m"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Stosując w jednej strefie pożarowej stałe urządzenia wodne gaśnicze strefę tę można powiększyć o:",
+        answers = listOf(
+            "50 %",
+            "25 %",
+            "100 %"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Ściany oddzielające pomieszczenie zagrożone wybuchem od innych pomieszczeń powinny być odporne na parcie o wartości:",
+        answers = listOf(
+            "25 kPa",
+            "15 kPa",
+            "35 kPa"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Podaj sposób reanimacji prowadzonej przez 1 ratownika:",
+        answers = listOf(
+            "15 na 4",
+            "10 na 2",
+            "15 na 2"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Co to jest „Pyran”?",
+        answers = listOf(
+            "środek ognioochronny",
+            "szkło zbrojone",
+            "szkło ognioodporne"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Kto powołuje Komendanta Głównego Państwowej Straży Pożarnej?",
+        answers = listOf(
+            "Prezydent RP na wniosek Ministra Spraw Wewnętrznych i Administracji",
+            "Prezes Rady Ministrów na wniosek Ministra Spraw Wewnętrznych i Administracji",
+            "Minister Spraw Wewnętrznych i Administracji"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Do jakiej klasy niebezpieczeństwa pożarowego zaliczamy benzynę?",
+        answers = listOf(
+            "II",
+            "I",
+            "III"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Po jakim czasie najpóźniej ratownik ma rozpocząć czynności masażu serca i sztucznego oddychania od momentu zatrzymania się powyższych czynności?",
+        answers = listOf(
+            "natychmiast",
+            "1–2 minuty",
+            "3–5 minut"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "W 2002 r. pismo ZOSP RP „Strażak” obchodziło jubileusz:",
+        answers = listOf(
+            "120-lecie",
+            "110-lecie",
+            "100-lecie"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Acetylen posiada wzór chemiczny:",
+        answers = listOf(
+            "CH₄",
+            "C₂H₂",
+            "C₂H₅OH"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Związek OSP został powołany w roku:",
+        answers = listOf(
+            "1958",
+            "1956",
+            "1946"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "W jakiej odległości od skraju toru kolejowego lub drogi publicznej jest zabronione pozostawianie chrustu i gałęzi?",
+        answers = listOf(
+            "25 m",
+            "50 m",
+            "75 m"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Pasy przeciwpożarowe w lasach należy utrzymywać w stanie zapewniającym ich użyteczność:",
+        answers = listOf(
+            "tylko wiosną i jesienią",
+            "tylko wiosną i latem",
+            "przez cały rok"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Ustawianie stert i stogów w stosunku do budynków wykonanych z materiałów palnych jest dozwolone w odległości co najmniej:",
+        answers = listOf(
+            "20 m",
+            "15 m",
+            "30 m"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Nad pomieszczeniem zagrożonym wybuchem należy stosować:",
+        answers = listOf(
+            "betonowy dach ciężki",
+            "lekki dach",
+            "dach wykonany z płyt żelbetowych wzmocnionych stalową konstrukcją"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Pomieszczenie zagrożone wybuchem w budynkach wielokondygnacyjnych należy sytuować na:",
+        answers = listOf(
+            "najwyższej kondygnacji budynku",
+            "najniższej kondygnacji",
+            "bez znaczenia"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Co oznacza pojęcie „resuscytacja oddechowo-krążeniowa”?",
+        answers = listOf(
+            "odsysanie śluzu z górnych dróg oddechowych",
+            "prowadzenie sztucznego oddychania",
+            "prowadzenie sztucznego oddychania i zewnętrznego pośredniego masażu serca"
+        ),
+        correctIndex = 2
+    )
+
+)
