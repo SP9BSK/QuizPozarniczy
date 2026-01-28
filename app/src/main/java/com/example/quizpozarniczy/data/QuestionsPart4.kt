ed074a5c85285caa7f57f3fbcea309c4ac44e6b2
SP9BSK <ratownikxl@gmail.com>
2026-01-28 11:17:58 +0100

    Create QuestionsPart4.kt

---
+++ app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart4.kt
@@ -0,0 +1,187 @@
+package com.example.quizpozarniczy.data
+
+import com.example.quizpozarniczy.model.Question
+
+val questionsPart4 = listOf(
+
+    Question(
+        text = "W czasie akcji gaśniczej znak sygnalizacyjny wykonany przez strażaka prawą ręką odchyloną w bok w dół oznacza:",
+        answers = listOf(
+            "woda naprzód",
+            "woda stój",
+            "rota I (I woda)"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Lekka woda to:",
+        answers = listOf(
+            "środek niezbędny do wytwarzania piany lekkiej",
+            "nowoczesny wypełniacz poduszek amortyzujący upadek ludzi",
+            "środek gaśniczy skuteczny przy gaszeniu pożarów z grupy B"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Hydrant wewnętrzny 52 posiada wydajność nominalną:",
+        answers = listOf(
+            "1,0 dm³/s",
+            "2,5 dm³/s",
+            "3,0 dm³/s"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Z przewodów dymowych i spalinowych w budynkach opalanych paliwem płynnym należy usuwać zanieczyszczenia:",
+        answers = listOf(
+            "co najmniej 2 razy w roku",
+            "co najmniej 4 razy w roku",
+            "co najmniej raz do roku"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Resuscytacja:",
+        answers = listOf(
+            "śmierć kliniczna",
+            "zespół czynności ratunkowych",
+            "wstrząs powypadkowy"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Stacja paliw może być zlokalizowana od lasu w odległości:",
+        answers = listOf(
+            "50 m",
+            "20 m",
+            "30 m"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Ubranie ochronne typu ciężkiego chroni przed temperaturą:",
+        answers = listOf(
+            "200°C",
+            "900°C",
+            "1400°C"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Kromos to:",
+        answers = listOf(
+            "środek gaśniczy",
+            "środek ognioochronny",
+            "środek zwilżający"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Ile butli z gazem płynnym o zawartości do 11 kg może być stosowanych jednocześnie w mieszkaniu?",
+        answers = listOf(
+            "1",
+            "2",
+            "3"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Do gazów gaśniczych nie zalicza się:",
+        answers = listOf(
+            "etan",
+            "CO₂",
+            "azot"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Co to jest strefa pożarowa?",
+        answers = listOf(
+            "strefa objęta pożarem",
+            "pomieszczenie zamykane drzwiami",
+            "wydzielona pożarowo przestrzeń"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "W przeciwpożarowy wyłącznik prądu należy wyposażać budynki:",
+        answers = listOf(
+            "użyteczności publicznej",
+            "których kubatura przekracza 1000 m³",
+            "produkcyjno-magazynowe"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Eksplozymetr służy do:",
+        answers = listOf(
+            "pomiaru stężeń gazu i par cieczy w powietrzu",
+            "tłumienia fali detonacyjnej",
+            "pomiaru temperatury zapłonu"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Minimalna głębokość przeciwpożarowego zbiornika wodnego w miejscu czerpania powinna wynosić:",
+        answers = listOf(
+            "0,5 m",
+            "1 m",
+            "1,5 m"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Podaj obowiązującą jednostkę miary odporności ogniowej elementów budowlanych:",
+        answers = listOf(
+            "godziny",
+            "minuty",
+            "nie stosuje się"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Drzwi powinny otwierać się na zewnątrz pomieszczeń, w których może przebywać jednocześnie:",
+        answers = listOf(
+            "50 osób",
+            "więcej niż 50 osób",
+            "nie stawia się wymagań"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Jak przed wojną nazywano kobiety działające w szeregach OSP?",
+        answers = listOf(
+            "ochotniczki",
+            "druhny",
+            "samarytanki"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Co oznacza skrót NRO?",
+        answers = listOf(
+            "element budynku najlepiej rozprzestrzeniający ogień",
+            "element budynku nie rozprzestrzeniający ognia",
+            "nie stosuje się takiego skrótu"
+        ),
+        correctIndex = 1
+    )
+
+)
