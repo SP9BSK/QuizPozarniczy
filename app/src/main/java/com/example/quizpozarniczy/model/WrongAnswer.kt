73afa75d109b153db51af9ed3eec823aebeac73e
SP9BSK <ratownikxl@gmail.com>
2026-01-29 16:54:21 +0100

    Update WrongAnswer.kt

--- app/src/main/java/com/example/quizpozarniczy/model/WrongAnswer.kt
+++ app/src/main/java/com/example/quizpozarniczy/model/WrongAnswer.kt
@@ -2,7 +2,7 @@ package com.example.quizpozarniczy.model
 
 data class WrongAnswer(
     val question: String,
-    val answers: List<String>,   // A / B / C dokładnie jak w teście
-    val chosenIndex: Int,        // co zawodnik zaznaczył
-    val correctIndex: Int        // poprawna odpowiedź
+    val answers: List<String>,
+    val chosenIndex: Int,
+    val correctIndex: Int
 )
