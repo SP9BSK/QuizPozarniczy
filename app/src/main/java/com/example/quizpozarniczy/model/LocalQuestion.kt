f250053187abe005f3df4b73ecc357721a1a38bc
SP9BSK <ratownikxl@gmail.com>
2026-01-30 14:50:02 +0100

    Update LocalQuestion.kt

--- app/src/main/java/com/example/quizpozarniczy/model/LocalQuestion.kt
+++ app/src/main/java/com/example/quizpozarniczy/model/LocalQuestion.kt
@@ -2,7 +2,13 @@ package com.example.quizpozarniczy.model
 
 data class LocalQuestion(
     val id: Int,
-    val question: String,
+    val prefix: String,          // tekst PRZED cudzysłowem
+    var quotedValue: String,     // 🔥 EDYTOWALNE
+    val suffix: String,          // tekst PO cudzysłowie
     val answers: MutableList<String>,
     var correctIndex: Int
-)
+) {
+    fun fullQuestion(): String {
+        return "$prefix „$quotedValue”$suffix"
+    }
+}
