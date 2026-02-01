0bf208ef35114eaefc28048dcbb3be3df7f45ff7
SP9BSK <ratownikxl@gmail.com>
2026-02-01 16:01:43 +0100

    Create QuestionsPart11.kt

---
+++ app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart11.kt
@@ -0,0 +1,228 @@
+package com.example.quizpozarniczy.data
+
+import com.example.quizpozarniczy.model.Question
+
+val questionsPart11 = listOf(
+
+    Question(
+        text = "Czy blaszane pokrycie dachu może być elementem instalacji odgromowej?",
+        answers = listOf(
+            "Nie, bo musi być wykonane z prętu stalowego ocynkowanego",
+            "Tak, pod warunkiem zastosowania blachy aluminiowej",
+            "Tak, pod warunkiem zastosowania blachy stalowej ocynkowanej nie cieńszej niż 0,5 mm"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Kto napisał słowa hymnu ZOSP RP?",
+        answers = listOf(
+            "Jerzy Skokowski",
+            "Jan Stokowski",
+            "Jakub Szela"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Czy klapy dymowe w dachach i stropodachach mogą być wykonane z materiałów łatwopalnych?",
+        answers = listOf(
+            "Tak",
+            "Nie",
+            "Tak, po uprzedniej impregnacji"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Klatki schodowe obudowane i zamykane drzwiami należy stosować w budynkach produkcyjnych i magazynowych:",
+        answers = listOf(
+            "Wysokościowych",
+            "Średniowysokich",
+            "Wysokich"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Powierzchnia strefy pożarowej w garażu zamkniętym – podziemnym nie może przekraczać:",
+        answers = listOf(
+            "1000 m²",
+            "500 m²",
+            "2500 m²"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Kto w 1184 roku przekazał relikwie św. Floriana do Krakowa?",
+        answers = listOf(
+            "Papież Lucjusz III",
+            "Biskup Krakowski Gedko",
+            "Stanisław Arczyński"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Jednym z najwybitniejszych członków Korpusu Technicznego Pożarnictwa był:",
+        answers = listOf(
+            "płk. Józef Lewandowski",
+            "mgr Aleksander Piotrowski",
+            "inż. Józef Tuliszkowski"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "W którym roku Centralne Muzeum Pożarnictwa w Mysłowicach otrzymało swój statut?",
+        answers = listOf(
+            "W styczniu 1982 roku",
+            "W lutym 1993 roku",
+            "W sierpniu 1975 roku"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Geometryczna wysokość podnoszenia to:",
+        answers = listOf(
+            "Suma geometrycznej wysokości ssania i tłoczenia, wyrażona w metrach",
+            "Suma geometrycznej wysokości ssania i tłoczenia wyrażona w litrach",
+            "Suma geometrycznej wysokości ssania wyrażona w metrach"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "II stopień oparzenia to:",
+        answers = listOf(
+            "Zaczerwienienie skóry, obrzęk i uczucie pieczenia",
+            "Pojawienie się pęcherzy z żółtawym płynem surowiczym i ostry ból",
+            "Martwica całej grubości skóry i uszkodzenie tkanek głębokich"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Główny Sąd Honorowy OSP RP składa się z:",
+        answers = listOf(
+            "Od 1 do 6 członków",
+            "Od 7 do 11 członków",
+            "Od 12 do 16 członków"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "W 1982 r. pismo ZOSP RP „Strażak” obchodziło jubileusz:",
+        answers = listOf(
+            "120-lecie",
+            "110-lecie",
+            "100-lecie"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Jaki wzór chemiczny ma fenol?",
+        answers = listOf(
+            "C6H5OH",
+            "CH2O",
+            "CH3Cl"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "W którym roku ZOSP obchodził 55-lecie?",
+        answers = listOf(
+            "2014 r.",
+            "2011 r.",
+            "2012 r."
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Budynki wysokościowe to budynki o wysokości:",
+        answers = listOf(
+            "Powyżej 55 m",
+            "Powyżej 75 m",
+            "Powyżej 25 m"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Ciężar w gotowości bojowej pojazdu pożarniczego GCBA 6/32 wynosi:",
+        answers = listOf(
+            "10 580 kg",
+            "12 620 kg",
+            "15 400 kg"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Co zawiera w swoim wyposażeniu zestaw ratunkowy R1 PSP?",
+        answers = listOf(
+            "Respirator",
+            "Worek samorozprężalny, rezerwuar tlenu i przewód łączący",
+            "Komplet masek twarzowych połączonych z respiratorem"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Żar żółtoczerwony ma temperaturę:",
+        answers = listOf(
+            "850–950 °C",
+            "1050–1150 °C",
+            "1250–1350 °C"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Gazy tworzące mieszaniny wybuchowe z powietrzem przy stężeniu do 10% objętości to:",
+        answers = listOf(
+            "Acetylen, wodór",
+            "Amoniak, argon",
+            "Tlen, sprężone powietrze"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "W którym roku skonstruowano pierwszy krajowy samochód pożarniczy Star A20 GBM 2/8?",
+        answers = listOf(
+            "1937 r.",
+            "1947 r.",
+            "1953 r."
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Przewody elektryczne można ciąć nożycami z izolowanymi rękojeściami, gdy napięcie nie przekracza:",
+        answers = listOf(
+            "220 V",
+            "300 V",
+            "500 V"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Ile wynosi nominalna wydajność hydrantu wewnętrznego o średnicy 52 mm?",
+        answers = listOf(
+            "1 dm³/s",
+            "2,5 dm³/s",
+            "3,5 dm³/s"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Na jakiej wysokości powinny być i
