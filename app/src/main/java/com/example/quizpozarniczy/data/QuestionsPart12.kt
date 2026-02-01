8f5e7535998a6d801cee3207cf1e7226eb1e2381
SP9BSK <ratownikxl@gmail.com>
2026-02-01 16:08:55 +0100

    Create QuestionsPart12.kt

---
+++ app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart12.kt
@@ -0,0 +1,48 @@
+package com.example.quizpozarniczy.data
+
+import com.example.quizpozarniczy.model.Question
+
+val questionsPart12 = listOf(
+
+    Question(
+        text = "Państwowa Straż Pożarna została powołana z dniem:",
+        answers = listOf(
+            "1 sierpnia 1992 r.",
+            "1 lipca 1992 r.",
+            "1 lipca 1991 r."
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Do krajowego Systemu Ratowniczo–Gaśniczego może zostać włączona jednostka, która posiada:",
+        answers = listOf(
+            "Co najmniej dwa średnie lub ciężkie samochody pożarnicze",
+            "Jeden samochód pożarniczy średni lub ciężki",
+            "Dwie motopompy M 8/8"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Który z wielkich Polaków ogłosił „Porządek Ogniowy”, będący pierwszym regulaminem gaszenia pożarów?",
+        answers = listOf(
+            "Kanclerz wielki koronny Jan Łaski",
+            "Marszałek izby poselskiej Jerzy Lubomirski",
+            "Marszałek Stanisław Lubomirski"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Budynki IN to:",
+        answers = listOf(
+            "Budynki produkcyjne i magazynowe",
+            "Budynki inwentarskie służące do hodowli inwentarza",
+            "Nie ma takiego określenia"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Budynki szkolne najczęściej zalicza się do
