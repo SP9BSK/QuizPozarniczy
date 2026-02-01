de59e9cb169218ac7dd9ad677de0b8e6c684952f
SP9BSK <ratownikxl@gmail.com>
2026-02-01 15:51:45 +0100

    Create QuestionsPart10.kt

---
+++ app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart10.kt
@@ -0,0 +1,164 @@
+package com.example.quizpozarniczy.data
+
+import com.example.quizpozarniczy.model.Question
+
+val questionsPart10 = listOf(
+
+    Question(
+        text = "W którym miejscu występuje najwyższa temperatura płomienia?",
+        answers = listOf(
+            "Na obrzeżach płomienia",
+            "Na szczycie płomienia",
+            "We wnętrzu płomienia"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Środki zwilżające są to środki, które:",
+        answers = listOf(
+            "Zwiększają napięcie powierzchniowe wody",
+            "Zmniejszają napięcie powierzchniowe wody",
+            "Obniżają temperaturę wody w celu zwiększenia jej właściwości gaśniczych"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Ekoperl to:",
+        answers = listOf(
+            "Nazwa pompy do ratownictwa chemiczno–ekologicznego",
+            "Sorbent – środek wiążący substancje niebezpieczne",
+            "Urządzenie do szybkiego mierzenia ciśnienia"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Przedłużająca się inhalacja organizmu człowieka czystym tlenem przy ciśnieniu atmosferycznym może prowadzić do:",
+        answers = listOf(
+            "Obrzęku płuc",
+            "Nie ma wpływu na organizm człowieka",
+            "Zawału"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Czujnik bezruchu to:",
+        answers = listOf(
+            "Sprzęt ratowniczy dla poszkodowanych oczekujących w bezruchu na noszach",
+            "Sprzęt osobisty ratownika służący do jego bezpieczeństwa w warunkach działań ratowniczych",
+            "Czujnik określający czy pojazd ratowniczy jest w ruchu"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Gdzie sprawdzamy tętno u niemowlęcia?",
+        answers = listOf(
+            "Na tętnicy przedramiennej",
+            "Na tętnicy szyjnej",
+            "W pachwinach"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Klasa odporności pożarowej dotyczy:",
+        answers = listOf(
+            "Budynku",
+            "Elementów budynku",
+            "Tylko oddzieleń przeciwpożarowych"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Odległość hydrantu zewnętrznego od ścian budynku powinna wynosić:",
+        answers = listOf(
+            "2–10 m",
+            "Najmniej 5 m",
+            "Do 5 m"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Minimalna odległość między butlą z gazem propan–butan a urządzeniem grzewczym powinna wynosić:",
+        answers = listOf(
+            "1,5 m",
+            "2 m",
+            "1,8 m"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Co stanie się z płomieniem świecy, jeżeli w pomieszczeniu zwiększymy stężenie tlenu?",
+        answers = listOf(
+            "Płomień skróci się i rozszerzy",
+            "Płomień wydłuży się",
+            "Płomień zgaśnie"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Masowa szybkość spalania odnosi się do:",
+        answers = listOf(
+            "Materiałów stałych",
+            "Cieczy",
+            "Gazów"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Ustawa „Prawo ochrony środowiska” została uchwalona:",
+        answers = listOf(
+            "27 kwietnia 2001 r.",
+            "3 listopada 1999 r.",
+            "24 sierpnia 1991 r."
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Funkcjonariusz PSP wykonujący czynności kontrolno–rozpoznawcze może ukarać kontrolowanego poprzez:",
+        answers = listOf(
+            "Skierowanie wniosku do kolegium ds. wykroczeń",
+            "Skierowanie wniosku do Sądu Grodzkiego",
+            "Skierowanie wniosku na Policję"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Jaki gaz o właściwościach wybuchowych wydziela się podczas ładowania akumulatorów zasadowych?",
+        answers = listOf(
+            "Metan",
+            "Wodór",
+            "Nie wydziela się żaden gaz"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Rurka wskaźnikowa w ratownictwie chemicznym służy do:",
+        answers = listOf(
+            "Ustalenia ilości substancji niebezpiecznej",
+            "Ustalenia rodzaju substancji niebezpiecznej",
+            "Ustalenia ilości zebranej substancji niebezpiecznej"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Wzrost koncentracji tlenu w obszarze spalania powoduje:",
+        answers = listOf(
+            "Zwiększenie minimalnej energii zapłonu",
+            "Podwyższenie górnych granic wybuchowości",
+            "Obniżenie górnych granic wybuchowości"
+        ),
+        correctIndex = 1
