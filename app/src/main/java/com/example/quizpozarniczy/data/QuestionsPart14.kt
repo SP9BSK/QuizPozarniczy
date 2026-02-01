e5ceaee45efc91188f8d4725706884384a2270d0
SP9BSK <ratownikxl@gmail.com>
2026-02-01 16:29:38 +0100

    Create QuestionsPart14.kt

---
+++ app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart14.kt
@@ -0,0 +1,160 @@
+package com.example.quizpozarniczy.data
+
+import com.example.quizpozarniczy.model.Question
+
+val questionsPart14 = listOf(
+
+    Question(
+        text = "Instalacja wentylacji oddymiającej powinna zapewniać:",
+        answers = listOf(
+            "Usuwanie dymu z intensywnością co najmniej 10 wymian na godzinę",
+            "Usuwanie dymu z intensywnością co najmniej 5 wymian na godzinę",
+            "Nie ma takiego wymogu"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Zasysacz liniowy pozwala na uzyskanie wodnego roztworu środka pianotwórczego o wartości:",
+        answers = listOf(
+            "3 i 6 %",
+            "6 i 12 %",
+            "13 i 16 %"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Drogi pożarowe o odpowiednio utwardzonej i wytrzymałej nawierzchni powinny być doprowadzone do obiektów niskich:",
+        answers = listOf(
+            "ZL III i ZL V",
+            "Nie wymaga się doprowadzenia dróg pożarowych do obiektów niskich",
+            "ZL I i ZL II"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Nudności i wymioty u poszkodowanego to:",
+        answers = listOf(
+            "Normalna reakcja na wypadek",
+            "Reakcja organizmu występująca zawsze przy urazie brzucha",
+            "Niebezpieczny objaw neurologiczny"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Ile wynosi czas ochronnego działania aparatu powietrznego AP-3 przy zużyciu 30 l/min?",
+        answers = listOf(
+            "53 min",
+            "38 min",
+            "60 min"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Wymień trzy typy dowodzenia działaniami ratowniczymi:",
+        answers = listOf(
+            "Operacyjny, taktyczny, współdziałający",
+            "Strategiczny, taktyczny, interwencyjny",
+            "Strategiczny, operacyjny, taktyczno–interwencyjny"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Przy zwichnięciach w stawie barkowym wykonujemy następujące czynności ratownicze:",
+        answers = listOf(
+            "Nastawiamy na miejscu zdarzenia",
+            "Unieruchamiamy w pozycji zastanej",
+            "Unieruchamiamy w pozycji fizjologicznej"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Zespół startujący w konkurencji – ćwiczenia bojowe podczas Mistrzostw Polski w Sporcie Pożarniczym liczy:",
+        answers = listOf(
+            "4 zawodników",
+            "7 zawodników",
+            "8 zawodników"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Energia cieplna (MJ) przypadająca na jednostkę powierzchni obiektu to:",
+        answers = listOf(
+            "Obciążenie ogniowe",
+            "Gęstość obciążenia ogniowego",
+            "Wielkość pożaru"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Do samochodów pożarniczych średnich zalicza się samochody o masie całkowitej:",
+        answers = listOf(
+            "Od 3,5 do 12 ton",
+            "Od 2 do 3,5 ton",
+            "Od 12 do 15 ton"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Względny czas trwania pożaru to:",
+        answers = listOf(
+            "Czas, w którym ulegną spaleniu materiały palne znajdujące się w pomieszczeniu lub strefie pożarowej",
+            "Czas, w którym ulegną spaleniu wszystkie materiały znajdujące się w pomieszczeniu lub strefie pożarowej",
+            "Czas, w którym ulegną spaleniu tylko stałe materiały palne znajdujące się w pomieszczeniu"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Wysoko umieszczone oświetlenie podstawowe i/lub ewakuacyjne powinno być usytuowane powyżej:",
+        answers = listOf(
+            "180 cm od podłogi",
+            "200 cm od podłogi",
+            "220 cm od podłogi"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Aparat oddechowy na sprężone powietrze należy do sprzętu:",
+        answers = listOf(
+            "Oczyszczającego",
+            "Filtrującego",
+            "Izolującego"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Liczba spienienia to:",
+        answers = listOf(
+            "Stosunek ilości piany do ilości środka pianotwórczego, z którego została wytworzona",
+            "Liczba pęcherzyków powietrza w 1 dm³ piany",
+            "Stosunek ilości wodnego roztworu środka pianotwórczego do wydajności piany"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Jaka najmniejsza ilość pomp musi być użyta w systemie przetłaczania wody?",
+        answers = listOf(
+            "2",
+            "1",
+            "3"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "CKG są to ubrania:",
+        answers = listOf(
+            "Ochrony przeciwchem
