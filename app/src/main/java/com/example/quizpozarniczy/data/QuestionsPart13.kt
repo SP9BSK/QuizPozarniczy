66a5da2153e7b39d6c1aaa9d53af87da49fa0f7b
SP9BSK <ratownikxl@gmail.com>
2026-02-01 16:20:07 +0100

    Create QuestionsPart13.kt

---
+++ app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart13.kt
@@ -0,0 +1,158 @@
+package com.example.quizpozarniczy.data
+
+import com.example.quizpozarniczy.model.Question
+
+val questionsPart13 = listOf(
+
+    Question(
+        text = "Podstawowym mechanizmem gaśniczym proszku jest działanie:",
+        answers = listOf(
+            "Chłodzące",
+            "Tłumiące",
+            "Izolujące"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Okładziny sufitów oraz sufity podwieszane w budynkach, z wyjątkiem mieszkań, należy wykonywać z:",
+        answers = listOf(
+            "Materiałów nierozprzestrzeniających ognia",
+            "Materiałów trudnozapalnych i nierozprzestrzeniających ognia",
+            "Materiałów niepalnych i niezapalnych, niekapiących i nieodpadających pod wpływem ognia"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Co to jest wstrząs pourazowy?",
+        answers = listOf(
+            "Uderzenie po upadku",
+            "Reakcja organizmu na uraz",
+            "Utrata przytomności"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Obowiązującą jednostką gęstości obciążenia ogniowego jest:",
+        answers = listOf(
+            "J/m²",
+            "MJ/m³",
+            "MJ/m²"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "W którym roku w Polsce rozpoczęto produkcję motopomp?",
+        answers = listOf(
+            "1933",
+            "1927",
+            "1929"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "W instalację odgromową podstawową muszą być wyposażone:",
+        answers = listOf(
+            "Budynki w zwartej zabudowie o wysokości powyżej 20 m i powierzchni ponad 250 m²",
+            "Budynki przeznaczone dla ludzi o ograniczonej zdolności poruszania się",
+            "Hale o wymiarach przekraczających 15 × 15 m z wewnętrznymi słupami żelbetowymi lub stalowymi"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Co oznacza skrót WSKR?",
+        answers = listOf(
+            "Wojewódzkie Stanowisko Koordynacji Ratownictwa",
+            "Wojskowy Sztab Kryzysowo–Ratowniczy",
+            "Wojewódzka Stacja Krwiodawstwa Ratunkowego"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Co uzyskujemy w konfuzorze?",
+        answers = listOf(
+            "Zmniejszenie prędkości przepływu",
+            "Zwiększenie prędkości przepływu",
+            "Zmniejszenie wydatku"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Substancją palną wydzielającą podczas spalania dym niebieskawy jest:",
+        answers = listOf(
+            "Drewno",
+            "Magnez",
+            "Siarka"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Na terenie którego województwa zorganizowano w 1945 r. pierwszy „Tydzień Obrony Przeciwpożarowej”?",
+        answers = listOf(
+            "Warszawskiego",
+            "Krakowskiego",
+            "Lubuskiego"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Płukanie uszkodzonej skóry wodą przez 20–30 minut stosujemy w przypadku:",
+        answers = listOf(
+            "Wyłącznie oparzeń termicznych",
+            "Silnego krwawienia",
+            "Oparzeń termicznych i chemicznych"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Hydrant wewnętrzny 52 posiada wydajność nominalną:",
+        answers = listOf(
+            "2,5 dm³/s",
+            "3,0 dm³/s",
+            "1,5 dm³/s"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Compakt to:",
+        answers = listOf(
+            "Nazwa pompy do ratownictwa chemiczno–ekologicznego",
+            "Podstawowy sprzęt wysokościowy",
+            "Sorbent – środek wiążący substancje niebezpieczne"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Piana gaśnicza o liczbie spienienia Ls = 40 jest pianą:",
+        answers = listOf(
+            "Lekką",
+            "Ciężką",
+            "Średnią"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Budynki zaliczone do kategorii zagrożenia ludzi ZL III to:",
+        answers = listOf(
+            "Budynki mieszkalne",
+            "Budynki użyteczności publicznej niezaliczone do ZL I i ZL II",
+            "Domy pomocy społecznej"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Obowiązujące rozporządzenie w sprawie warunków technicznych, jakim powinny odpowi
