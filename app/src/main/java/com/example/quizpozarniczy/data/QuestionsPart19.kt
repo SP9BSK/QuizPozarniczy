32551132f6b873657efb8dec412b772f83536691
SP9BSK <ratownikxl@gmail.com>
2026-02-03 15:54:17 +0100

    Create QuestionsPart19.kt

---
+++ app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart19.kt
@@ -0,0 +1,246 @@
+package com.example.quizpozarniczy.data
+
+import com.example.quizpozarniczy.model.Question
+
+val questionsPart19 = listOf(
+
+    Question(
+        text = "Czy w jednokondygnacyjnym garażu podziemnym wymagane jest stosowanie stałych urządzeń gaśniczych?",
+        answers = listOf(
+            "tak, jeżeli powierzchnia strefy pożarowej w garażu przekracza 1500 m²",
+            "tak, jeżeli w strefie pożarowej znajduje się więcej niż 100 stanowisk postojowych",
+            "nie jest wymagane"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Czy w lesie o szerokości mniejszej niż 200 m istnieje obowiązek utrzymywania pasów przeciwpożarowych?",
+        answers = listOf(
+            "tak",
+            "nie",
+            "tak, jeżeli przez las przebiega droga publiczna lub linia kolejowa"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Kiedy należy oznakować pojazd przewożący butle z gazem płynnym propan-butan tablicami barwy pomarańczowej?",
+        answers = listOf(
+            "gdy przewozi przynajmniej jedną butlę 11 kg",
+            "gdy przewozi 330 butli 11 kg",
+            "niezależnie od ilości gazu każdy transport powinien być oznakowany"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Ile wynosi wymagana ilość wody do celów przeciwpożarowych dla stodoły o powierzchni powyżej 2000 m²?",
+        answers = listOf(
+            "co najmniej 5 dm³/s",
+            "co najmniej 10 dm³/s",
+            "co najmniej 15 dm³/s"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Czy pomiędzy drogą pożarową a budynkiem mogą znajdować się drzewa?",
+        answers = listOf(
+            "tak",
+            "nie",
+            "tak, pod warunkiem, że ich wysokość nie przekracza 3 m"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Czy klapy dymowe muszą posiadać klasę odporności ogniowej wymaganą dla danego budynku?",
+        answers = listOf(
+            "tak",
+            "nie",
+            "tak, jeżeli są stosowane w budynkach produkcyjnych i magazynowych o powierzchni strefy pożarowej przekraczającej 1000 m²"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Czy do celów ewakuacyjnych mogą być stosowane drzwi obrotowe?",
+        answers = listOf(
+            "nie",
+            "tak",
+            "tak, pod warunkiem zapewnienia samoczynnego złożenia skrzydeł i pozostawienia ich w pozycji otwartej w razie pożaru"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Państwowe Gospodarstwo Leśne Lasy Państwowe gospodaruje na jakiej części powierzchni wszystkich lasów w Polsce?",
+        answers = listOf(
+            "78 procent powierzchni wszystkich lasów w Polsce",
+            "53 procent",
+            "32 procent"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Powierzchnia wszystkich lasów w Polsce wynosi ok. 9 mln hektarów, co odpowiada lesistości:",
+        answers = listOf(
+            "37%",
+            "28%",
+            "19%"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Opracowanie i aktualizację powiatowego planu ratowniczego koordynuje:",
+        answers = listOf(
+            "komendant wojewódzki PSP",
+            "komendant powiatowy (miejski) PSP",
+            "rada powiatu"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Wojewódzkie plany ratownicze zatwierdza:",
+        answers = listOf(
+            "komendant wojewódzki PSP",
+            "wojewoda",
+            "wojewódzki sejmik samorządowy"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Podstawowy zespół ratownictwa medycznego to zespół, w którego skład wchodzą co najmniej:",
+        answers = listOf(
+            "dwie osoby uprawnione do udzielania pierwszej pomocy medycznej, działające na polecenie i w porozumieniu z lekarzem",
+            "trzy osoby uprawnione do samodzielnego udzielania pierwszej pomocy medycznej",
+            "jeden lekarz i dwie osoby uprawnione do udzielania pomocy medycznej"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Siły i środki wyznaczone do składu centralnego obwodu operacyjnego nie mogą przekraczać:",
+        answers = listOf(
+            "jednej drugiej sił Państwowej Straży Pożarnej województwa",
+            "jednej drugiej sił ratowniczych województwa",
+            "jednej trzeciej sił Państwowej Straży Pożarnej województwa"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Batalion to:",
+        answers = listOf(
+            "związek pododdziałów i oddziałów przewidziany do prowadzenia działań ratowniczych na terenie całego województwa",
+            "oddział w sile od trzech do pięciu kompanii oraz dowódca",
+            "związek specjalistycznych grup ratowniczych przewidzianych do działań na terenie własnego województwa"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Wojewódzkie odwody operacyjne zapewniają możliwość prowadzenia działań ratowniczych bez dodatkowego zaprowiantowania przez okres co najmniej:",
+        answers = listOf(
+            "72 godziny",
+            "48 godzin",
+            "24 godziny"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Powołanie i odwołanie ze stanowisk dowódczych w wojewódzkich brygadach odwodowych należy do:",
+        answers = listOf(
+            "komendanta głównego PSP",
+            "dowódcy wojewódzkiej brygady",
+            "dowódcy centralnego obwodu operacyjnego"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "W skład jednostek organizacyjnych PSP wchodzą:",
+        answers = listOf(
+            "jednostki ratowniczo-gaśnicze",
+            "terenowe służby ratownicze",
+            "zakładowe służby ratownicze"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "W korpusie podoficerów straży pożarnej czwartym stopniem jest:",
+        answers = listOf(
+            "starszy sekcyjny",
+            "ogniomistrz",
+            "starszy aspirant sztabowy"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Szkoła Główna Służby Pożarniczej powstała w roku:",
+        answers = listOf(
+            "1975",
+            "1981",
+            "1982"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "XI Mistrzostwa Polski w Sportach Pożarniczych odbyły się:",
+        answers = listOf(
+            "we wrześniu 1994 r. w Krakowie",
+            "w maju 1995 r. w Warszawie",
+            "w październiku 1994 r. w Poznaniu"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Służba strażacka w służbie przygotowawczej trwa:",
+        answers = listOf(
+            "rok",
+            "dwa lata",
+            "trzy lata"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Wykonawcą piosenki „Płonąca stodoła” był:",
+        answers = listOf(
+            "Krzysztof Krawczyk",
+            "Czesław Niemen",
+            "Stan Borys"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Gdzie w Polsce odbywa się Ogólnopolski Festiwal Orkiestr?",
+        answers = listOf(
+            "w Ciechocinku",
+            "w Ciechanowie",
+            "w Niepokalanowie"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Strażak wybrany do organów ZOSP:",
+        answers = listOf(
+            "traci uprawnienia strażaka",
+            "nie traci uprawnień strażaka",
+            "zawiesza się jego uprawnienia na czas kadencji"
+        ),
+        correctIndex = 1
+    )
+)
