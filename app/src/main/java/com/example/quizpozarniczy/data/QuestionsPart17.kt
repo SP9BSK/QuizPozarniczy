fb0e82ea63cc67c7abc2dd631160f57359508fb5
SP9BSK <ratownikxl@gmail.com>
2026-02-03 12:17:40 +0100

    Create QuestionsPart17.kt

---
+++ app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart17.kt
@@ -0,0 +1,221 @@
+object QuestionsPart17 {
+
+    val questions = listOf(
+        Question(
+            id = 402,
+            text = "Przy spalaniu gazów mamy do czynienia z różnymi rodzajami płomieni. Jaką nazwę nosi płomień, gdy z palnika wydobywa się gaz nie zmieszany z powietrzem lub tlenem, a powietrze (tlen) dociera do niego w stanie spalania?",
+            answers = listOf(
+                "płomień zewnętrzny",
+                "płomień kinetyczny",
+                "płomień dyfuzyjny"
+            ),
+            correctIndex = 2
+        ),
+        Question(
+            id = 403,
+            text = "Pożar budynku o objętości 400 m² to pożar:",
+            answers = listOf("mały", "średni", "duży"),
+            correctIndex = 1
+        ),
+        Question(
+            id = 404,
+            text = "Jakie średnice mają węże ssawne stosowane w ochronie przeciwpożarowej?",
+            answers = listOf(
+                "110 mm",
+                "75 mm i 110 mm",
+                "52 mm, 75 mm i 110 mm"
+            ),
+            correctIndex = 2
+        ),
+        Question(
+            id = 405,
+            text = "Parametry prądownicy pianowej PP2 to:",
+            answers = listOf(
+                "nasada tłoczna 52 mm i wydajność wodnego roztworu środka pianotwórczego 200 dm³/min",
+                "nasada tłoczna 52 mm i wydajność piany gaśniczej 400 dm³/min",
+                "taka prądownica nie występuje na wyposażeniu samochodów PSP i OSP"
+            ),
+            correctIndex = 0
+        ),
+        Question(
+            id = 406,
+            text = "Masa gotowej do użycia gaśnicy przenośnej nie powinna przekraczać:",
+            answers = listOf("9 kg", "12 kg", "20 kg"),
+            correctIndex = 2
+        ),
+        Question(
+            id = 407,
+            text = "Pododdział w sile 9 ratowników to:",
+            answers = listOf("zastęp", "sekcja", "pluton"),
+            correctIndex = 1
+        ),
+        Question(
+            id = 408,
+            text = "Podpinka, wchodząca w skład uzbrojenia osobistego strażaka, to:",
+            answers = listOf(
+                "tkanina wewnętrzna do izolacji cieplnej",
+                "system uchwytów do mocowania uzbrojenia",
+                "krótka linka z hakiem do zabezpieczenia linii wężowych"
+            ),
+            correctIndex = 2
+        ),
+        Question(
+            id = 409,
+            text = "Do ugaszenia pożaru zużyto 0,2 m³ roztworu środka pianotwórczego. Jaką objętość piany uzyskano?",
+            answers = listOf(
+                "200 dm³",
+                "2,4 dm³",
+                "15 dm³"
+            ),
+            correctIndex = 1
+        ),
+        Question(
+            id = 410,
+            text = "Obliczeniowy czas podawania piany ciężkiej wynosi:",
+            answers = listOf("5 min", "8 min", "10 min"),
+            correctIndex = 0
+        ),
+        Question(
+            id = 411,
+            text = "Czy każde stałe urządzenie gaśnicze zalicza się do urządzeń przeciwpożarowych?",
+            answers = listOf("tak", "nie", "tak, pod warunkiem stosowania tryskaczy"),
+            correctIndex = 0
+        ),
+        Question(
+            id = 412,
+            text = "Czy dopuszcza się garażowanie ciągnika rolniczego w budynku stodoły?",
+            answers = listOf(
+                "nie",
+                "tak, jeżeli postój nie przekracza 60 min",
+                "tak, jeżeli zbiornik paliwa jest opróżniony"
+            ),
+            correctIndex = 0
+        ),
+        Question(
+            id = 413,
+            text = "Czy na stacji paliw dopuszczalne jest napełnianie butli gazem płynnym?",
+            answers = listOf("nie", "tak", "tak, pod warunkiem"),
+            correctIndex = 0
+        ),
+        Question(
+            id = 414,
+            text = "Jaka maksymalna temperatura może występować w pomieszczeniu magazynowania butli z gazami płynnymi?",
+            answers = listOf("21°C", "25°C", "35°C"),
+            correctIndex = 2
+        ),
+        Question(
+            id = 415,
+            text = "Jak często należy przeprowadzać praktyczne sprawdzenie ewakuacji w sklepie powyżej 50 osób?",
+            answers = listOf(
+                "raz na dwa lata",
+                "raz na trzy lata",
+                "raz na pięć lat"
+            ),
+            correctIndex = 0
+        ),
+        Question(
+            id = 416,
+            text = "Czy w domu pomocy społecznej wymagany jest SSP?",
+            answers = listOf(
+                "nie",
+                "tak, jeżeli liczba łóżek przekracza 100",
+                "tak, przy przekroczeniu powierzchni strefy"
+            ),
+            correctIndex = 1
+        ),
+        Question(
+            id = 417,
+            text = "Czy miejsce omłotów powinno być wyposażone w pojemniki z wodą?",
+            answers = listOf(
+                "tak",
+                "tak, min. 200 dm³",
+                "nie, jeśli są gaśnice"
+            ),
+            correctIndex = 1
+        ),
+        Question(
+            id = 418,
+            text = "Jaką odległość należy zachować pomiędzy sterami w jednej strefie pożarowej?",
+            answers = listOf("20 m", "30 m", "nie normuje się"),
+            correctIndex = 2
+        ),
+        Question(
+            id = 419,
+            text = "Wymagana ilość wody do celów ppoż. dla stacji paliw wynosi:",
+            answers = listOf("5 dm³/s", "10 dm³/s", "20 dm³/s"),
+            correctIndex = 1
+        ),
+        Question(
+            id = 420,
+            text = "Czy w budynku jednorodzinnym konieczne jest wydzielenie pożarowe magazynu oleju opałowego?",
+            answers = listOf(
+                "tak",
+                "nie",
+                "tak, gdy pojemność przekracza 1000 m³"
+            ),
+            correctIndex = 1
+        ),
+        Question(
+            id = 421,
+            text = "Zbiorowisko leśne z dominacją drzew iglastych to:",
+            answers = listOf("ols", "grąd", "bór"),
+            correctIndex = 2
+        ),
+        Question(
+            id = 422,
+            text = "Największy zwarty kompleks leśny w Polsce to:",
+            answers = listOf(
+                "Bory Tucholskie",
+                "Puszcza Kozienicka",
+                "Puszcza Białowieska"
+            ),
+            correctIndex = 0
+        ),
+        Question(
+            id = 423,
+            text = "Na jakiej zasadzie organizacje pozarządowe wspomagają KSRG na poziomie powiatowym?",
+            answers = listOf("obowiązku", "dobrowolności", "zgody wojewody"),
+            correctIndex = 1
+        ),
+        Question(
+            id = 424,
+            text = "Kto koordynuje wojewódzki plan ratowniczy?",
+            answers = listOf(
+                "komendant wojewódzki PSP",
+                "komendant główny PSP",
+                "marszałek województwa"
+            ),
+            correctIndex = 0
+        ),
+        Question(
+            id = 425,
+            text = "Kto zatwierdza powiatowe plany ratownicze?",
+            answers = listOf(
+                "komendant powiatowy PSP",
+                "komendant wojewódzki PSP",
+                "starosta"
+            ),
+            correctIndex = 2
+        ),
+        Question(
+            id = 426,
+            text = "Jak często aktualizuje się powiatowe i wojewódzkie plany ratownicze?",
+            answers = listOf(
+                "co roku i w razie potrzeby",
+                "co 2 lata",
+                "co 3 lata"
+            ),
+            correctIndex = 0
+        ),
+        Question(
+            id = 427,
+            text = "Kto prowadzi kierowanie działaniem ratowniczym?",
+            answers = listOf(
+                "sztab",
+                "dowódca pierwszego podmiotu na miejscu",
+                "oficer policji"
+            ),
+            correctIndex = 1
+        )
+    )
+}
