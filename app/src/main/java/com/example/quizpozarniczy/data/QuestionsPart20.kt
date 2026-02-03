89671035ed12dcdc4aeaa20e2c3a259d4b89e317
SP9BSK <ratownikxl@gmail.com>
2026-02-03 16:04:29 +0100

    Create QuestionsPart20.kt

---
+++ app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart20.kt
@@ -0,0 +1,226 @@
+package com.example.quizpozarniczy.data
+
+import com.example.quizpozarniczy.model.Question
+
+val questionsPart20 = listOf(
+
+    Question(
+        text = "Koszt utrzymania, wyposażenia i zapewnienia gotowości bojowej ochotniczej straży pożarnej ponosi:",
+        answers = listOf(
+            "komenda powiatowa PSP",
+            "sołtys",
+            "gmina"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Bezpieczne stężenie gazu palnego w mieszaninie z powietrzem podczas prac z użyciem otwartego ognia nie może przekraczać:",
+        answers = listOf(
+            "10% dolnej granicy wybuchowej gazu",
+            "50% dolnej granicy wybuchowej",
+            "70% dolnej granicy wybuchowej"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Badania okresowe instalacji odgromowej w obiektach zagrożonych wybuchem należy przeprowadzać:",
+        answers = listOf(
+            "raz w roku przed sezonem burzowym nie później niż 15 kwietnia",
+            "raz w roku przed sezonem burzowym nie później niż 30 kwietnia",
+            "raz w roku"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "W co powinien być wyposażony kierowca cysterny przewożącej benzynę w ilości 19 000 litrów?",
+        answers = listOf(
+            "maska ucieczkowa z pochłaniaczem par benzyny, sorbent, kombinezon ochronny",
+            "saperka, latarka, kamizelka ostrzegawcza, 2 gaśnice proszkowe",
+            "latarka, kamizelka ostrzegawcza, 2 pachołki lub 2 trójkąty ostrzegawcze lub 2 lampy barwy pomarańczowej z własnym zasilaniem"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Wykładziny podłogowe stosowane na drogach komunikacji ogólnej służące celom ewakuacji powinny być co najmniej:",
+        answers = listOf(
+            "niepalne",
+            "trudno palne",
+            "palne"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Symbol F2 oznacza:",
+        answers = listOf(
+            "klasę odporności ogniowej elementów budynków",
+            "rodzaj środka pianotwórczego",
+            "powierzchnię pożaru równą 2000 m²"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Palenie podtrzymuje:",
+        answers = listOf(
+            "chlor",
+            "argon",
+            "dwutlenek węgla"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Najmniejsza wysokość drzwi w świetle na drogach ewakuacyjnych powinna wynosić:",
+        answers = listOf(
+            "190 cm",
+            "195 cm",
+            "200 cm"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Lekki dach to:",
+        answers = listOf(
+            "dach, z którego nie wolno prowadzić akcji gaśniczej",
+            "dach o ciężarze nieprzekraczającym 75 kg/m² rzutu wraz z konstrukcją nośną",
+            "dach z materiałów niepalnych lub trudno zapalnych o ciężarze nieprzekraczającym 75 kg/m² rzutu"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Butle z gazem propan–butan można stosować w budynkach o wysokości do:",
+        answers = listOf(
+            "15 m",
+            "25 m",
+            "55 m"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "W samochodzie gaśniczym GBA 2,5/16 zgodnie z regulaminem rozwinięcia znajdują się:",
+        answers = listOf(
+            "dwie wytwornice piany średniej WP-2-75 i jedna wytwornica WP-2-150",
+            "dwie wytwornice piany średniej WP-2-75 i dwie wytwornice WP-2-50",
+            "brak wyposażenia w wytwornice piany średniej"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Sekcja GCBA 6/32 „Jelcz” przy podaniu jednego prądu wody może rozwinąć się na maksymalną odległość:",
+        answers = listOf(
+            "560 m",
+            "360 m",
+            "400 m"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Aby mogła powstać mechaniczna piana gaśnicza niezbędne są:",
+        answers = listOf(
+            "woda, środek pianotwórczy, urządzenie",
+            "wodny roztwór środka pianotwórczego, gaz, urządzenie",
+            "woda, środek pianotwórczy, powietrze"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Zawór z zamknięciem grzybkowym i zamkiem rozpadającym się pod wpływem ciepła oraz rozpryskiwaczem nosi nazwę:",
+        answers = listOf(
+            "zraszacz",
+            "tryskacz",
+            "zawór alarmowo-gaśniczy"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Zjawisko inhibicji (antykatalizy) procesu spalania występuje podczas gaszenia pożarów:",
+        answers = listOf(
+            "halonami",
+            "parą wodną",
+            "dwutlenkiem węgla"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Planowany jest przewóz 100 kg dynamitu do kopalni. Na ile dni wcześniej należy zgłosić ten fakt do Komendanta Wojewódzkiego PSP?",
+        answers = listOf(
+            "14 dni",
+            "5 dni",
+            "2 dni"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Litera „X” w górnym rzędzie znaków na tablicy ostrzegawczej ADR oznacza:",
+        answers = listOf(
+            "uwaga, promieniowanie",
+            "środek znajduje się pod stałym ciśnieniem",
+            "absolutny zakaz kontaktu materiału z wodą"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        text = "Górne cyfry na tablicy ostrzegawczej ADR oznaczają:",
+        answers = listOf(
+            "numer identyfikacyjny przewożonej substancji",
+            "numer rozpoznawczy rodzaju niebezpieczeństwa substancji",
+            "sposób postępowania podczas awarii"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        text = "Pęcherzyk piany wytworzonej przez gaśnicę o symbolu GWP 9Y wypełnia:",
+        answers = listOf(
+            "CO₂",
+            "powietrze",
+            "azot"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "APL to:",
+        answers = listOf(
+            "agregat piany lekkiej",
+            "agregat proszkowy lekki",
+            "agregat proszkowy specjalny"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Ile nasad odprowadzających (wylotowych) Ø75 posiada rozdzielacz?",
+        answers = listOf(
+            "1",
+            "2",
+            "3"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        text = "Symbol PP 2-12 oznacza prądownicę:",
+        answers = listOf(
+            "wodną o wydajności 20 l/min",
+            "pianową o wydajności 12 m³/s",
+            "pianową o wydajności 200 l/min i liczbie spienienia 12"
+        ),
+        correctIndex = 2
+    )
+)
