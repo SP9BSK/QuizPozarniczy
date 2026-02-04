06dda9513d13426e28518f9ad51dcb08451aa154
SP9BSK <ratownikxl@gmail.com>
2026-02-04 12:23:16 +0100

    Update RegulaminActivity.kt

--- app/src/main/java/com/example/quizpozarniczy/RegulaminActivity.kt
+++ app/src/main/java/com/example/quizpozarniczy/RegulaminActivity.kt
@@ -1,6 +1,7 @@
 package com.example.quizpozarniczy
 
 import android.os.Bundle
+import android.view.WindowManager   // ⬅️ TO BYŁO POTRZEBNE
 import androidx.appcompat.app.AppCompatActivity
 
 class RegulaminActivity : AppCompatActivity() {
@@ -9,6 +10,7 @@ class RegulaminActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_regulamin)
 
+        // 🔥 NIE GAŚ EKRAN
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 }
