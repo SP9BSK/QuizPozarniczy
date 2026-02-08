d204e85ca260b34df298f63d5b99c0f742134c09
SP9BSK <ratownikxl@gmail.com>
2026-02-08 09:50:22 +0100

    Update QuizActivity.kt

--- app/src/main/java/com/example/quizpozarniczy/QuizActivity.kt
+++ app/src/main/java/com/example/quizpozarniczy/QuizActivity.kt
@@ -10,7 +10,7 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.quizpozarniczy.data.LocalQuestionsRepository
-import com.example.quizpozarniczy.model.PlayerResult
+import com.example.quizpozarniczy.PlayerResult
 import com.example.quizpozarniczy.model.Question
 import com.example.quizpozarniczy.model.WrongAnswer
 import kotlin.math.min
