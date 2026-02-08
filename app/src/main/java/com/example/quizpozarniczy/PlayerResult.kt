2146f08c2d5be3623c527af53bdc3dbc8ea11934
SP9BSK <ratownikxl@gmail.com>
2026-02-08 17:08:41 +0100

    Update PlayerResult.kt

--- app/src/main/java/com/example/quizpozarniczy/PlayerResult.kt
+++ app/src/main/java/com/example/quizpozarniczy/PlayerResult.kt
@@ -4,6 +4,7 @@ import com.example.quizpozarniczy.model.WrongAnswer
 
 data class PlayerResult(
     val playerNumber: Int,
+    val playerName: String,
     val score: Int,
     val total: Int,
     val timeSeconds: Int,
