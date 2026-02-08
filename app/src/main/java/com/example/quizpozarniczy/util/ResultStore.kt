db411da2e2a0b02c3d07f0740c673c1ceb8ae8ba
SP9BSK <ratownikxl@gmail.com>
2026-02-08 09:51:06 +0100

    Update ResultStore.kt

--- app/src/main/java/com/example/quizpozarniczy/util/ResultStore.kt
+++ app/src/main/java/com/example/quizpozarniczy/util/ResultStore.kt
@@ -1,6 +1,6 @@
 package com.example.quizpozarniczy.util
 
-import com.example.quizpozarniczy.model.PlayerResult
+import com.example.quizpozarniczy.PlayerResult
 
 object ResultStore {
     val results = mutableListOf<PlayerResult>()
