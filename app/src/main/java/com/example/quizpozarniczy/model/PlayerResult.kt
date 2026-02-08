41181ddcd0c5fd749d73d99bc9797c41f9f089af
SP9BSK <ratownikxl@gmail.com>
2026-02-08 07:28:48 +0100

    Delete app/src/main/java/com/example/quizpozarniczy/model/PlayerResult.kt

--- app/src/main/java/com/example/quizpozarniczy/model/PlayerResult.kt
+++
@@ -1,9 +0,0 @@
-package com.example.quizpozarniczy.model
-
-data class PlayerResult(
-    val playerNumber: Int,
-    val score: Int,
-    val total: Int,
-    val wrongAnswers: List<WrongAnswer>
-)
-
