6042ac8749978c21c0840430f1059adc531f63fb
SP9BSK <ratownikxl@gmail.com>
2026-02-08 13:23:11 +0100

    Update QuizSession.kt

--- app/src/main/java/com/example/quizpozarniczy/QuizSession.kt
+++ app/src/main/java/com/example/quizpozarniczy/QuizSession.kt
@@ -3,10 +3,18 @@ package com.example.quizpozarniczy
 object QuizSession {
     var totalPlayers = 1
     var currentPlayer = 1
+
+    val playerNames = mutableListOf<String>()
     val results = mutableListOf<PlayerResult>()
 
-    fun reset() {
+    fun reset(players: Int) {
+        totalPlayers = players
         currentPlayer = 1
         results.clear()
+
+        playerNames.clear()
+        repeat(players) {
+            playerNames.add("Zawodnik ${it + 1}")
+        }
     }
 }
