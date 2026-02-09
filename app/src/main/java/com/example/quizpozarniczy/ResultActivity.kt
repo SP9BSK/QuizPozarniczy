cb5e2d822c4958bfaeb6703478a85c40abb4c40b
SP9BSK <ratownikxl@gmail.com>
2026-02-09 11:42:29 +0100

    Update ResultActivity.kt

--- app/src/main/java/com/example/quizpozarniczy/ResultActivity.kt
+++ app/src/main/java/com/example/quizpozarniczy/ResultActivity.kt
@@ -7,7 +7,8 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class RankingActivity : AppCompatActivity() {
+class ResultActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
