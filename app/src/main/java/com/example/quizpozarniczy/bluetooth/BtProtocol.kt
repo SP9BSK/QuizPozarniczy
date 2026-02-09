8315c277b9b68522dfd8cfc287b88edc96092371
SP9BSK <ratownikxl@gmail.com>
2026-02-09 19:58:46 +0100

    Delete app/src/main/java/com/example/quizpozarniczy/bluetooth directory

--- app/src/main/java/com/example/quizpozarniczy/bluetooth/BtProtocol.kt
+++
@@ -1,8 +0,0 @@
-package com.example.quizpozarniczy.bluetooth
-
-import java.util.UUID
-
-object BtProtocol {
-    val APP_UUID: UUID =
-        UUID.fromString("a60f35f0-b93a-11de-8a39-08002009c666")
-}
