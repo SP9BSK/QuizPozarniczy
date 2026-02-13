58de56895e68a706b74e10b3d44e89f5605cc40c
SP9BSK <ratownikxl@gmail.com>
2026-02-13 13:08:16 +0100

    Update QuizActivity.kt

--- app/src/main/java/com/example/quizpozarniczy/QuizActivity.kt
+++ app/src/main/java/com/example/quizpozarniczy/QuizActivity.kt
@@ -12,6 +12,8 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.quizpozarniczy.model.Question
 import kotlin.math.min
+import com.example.quizpozarniczy.model.WrongAnswer
+
 
 class QuizActivity : AppCompatActivity() {
 
