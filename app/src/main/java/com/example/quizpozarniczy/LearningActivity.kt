46ddf2966672a727c5155108996b6c9a0dc15740
SP9BSK <ratownikxl@gmail.com>
2026-02-17 15:54:21 +0100

    Add flag to keep screen on during learning activity

--- app/src/main/java/com/example/quizpozarniczy/LearningActivity.kt
+++ app/src/main/java/com/example/quizpozarniczy/LearningActivity.kt
@@ -35,6 +35,8 @@ class LearningActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_learning)
 
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
         learningMode = intent.getStringExtra("LEARNING_MODE") ?: "GENERAL"
 
         txtProgress = findViewById(R.id.txtProgress)
