72dc34cd8181c92c6b1217614cf4c1f05f69d728
SP9BSK <ratownikxl@gmail.com>
2026-02-24 10:17:04 +0100

    Update QuestionsPart20.kt

--- app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart20.kt
+++ app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart20.kt
@@ -5,26 +5,21 @@ import com.example.quizpozarniczy.model.Question
 val questionsPart20 = listOf(
 
     Question(
+        id = 466,
         text = "Koszt utrzymania, wyposażenia i zapewnienia gotowości bojowej ochotniczej straży pożarnej ponosi:",
-        answers = listOf(
-            "komenda powiatowa PSP",
-            "sołtys",
-            "gmina"
-        ),
+        answers = listOf("komenda powiatowa PSP", "sołtys", "gmina"),
         correctIndex = 2
     ),
 
     Question(
+        id = 467,
         text = "Bezpieczne stężenie gazu palnego w mieszaninie z powietrzem podczas prac z użyciem otwartego ognia nie może przekraczać:",
-        answers = listOf(
-            "10% dolnej granicy wybuchowej gazu",
-            "50% dolnej granicy wybuchowej",
-            "70% dolnej granicy wybuchowej"
-        ),
+        answers = listOf("10% dolnej granicy wybuchowej gazu", "50% dolnej granicy wybuchowej", "70% dolnej granicy wybuchowej"),
         correctIndex = 0
     ),
 
     Question(
+        id = 468,
         text = "Badania okresowe instalacji odgromowej w obiektach zagrożonych wybuchem należy przeprowadzać:",
         answers = listOf(
             "raz w roku przed sezonem burzowym nie później niż 15 kwietnia",
@@ -35,6 +30,7 @@ val questionsPart20 = listOf(
     ),
 
     Question(
+        id = 469,
         text = "W co powinien być wyposażony kierowca cysterny przewożącej benzynę w ilości 19 000 litrów?",
         answers = listOf(
             "maska ucieczkowa z pochłaniaczem par benzyny, sorbent, kombinezon ochronny",
@@ -45,16 +41,14 @@ val questionsPart20 = listOf(
     ),
 
     Question(
+        id = 470,
         text = "Wykładziny podłogowe stosowane na drogach komunikacji ogólnej służące celom ewakuacji powinny być co najmniej:",
-        answers = listOf(
-            "niepalne",
-            "trudno palne",
-            "palne"
-        ),
+        answers = listOf("niepalne", "trudno palne", "palne"),
         correctIndex = 1
     ),
 
     Question(
+        id = 471,
         text = "Symbol F2 oznacza:",
         answers = listOf(
             "klasę odporności ogniowej elementów budynków",
@@ -65,46 +59,14 @@ val questionsPart20 = listOf(
     ),
 
     Question(
+        id = 472,
         text = "Palenie podtrzymuje:",
-        answers = listOf(
-            "chlor",
-            "argon",
-            "dwutlenek węgla"
-        ),
+        answers = listOf("chlor", "argon", "dwutlenek węgla"),
         correctIndex = 0
     ),
 
     Question(
-        text = "Najmniejsza wysokość drzwi w świetle na drogach ewakuacyjnych powinna wynosić:",
-        answers = listOf(
-            "190 cm",
-            "195 cm",
-            "200 cm"
-        ),
-        correctIndex = 2
-    ),
-
-    Question(
-        text = "Lekki dach to:",
-        answers = listOf(
-            "dach, z którego nie wolno prowadzić akcji gaśniczej",
-            "dach o ciężarze nieprzekraczającym 75 kg/m² rzutu wraz z konstrukcją nośną",
-            "dach z materiałów niepalnych lub trudno zapalnych o ciężarze nieprzekraczającym 75 kg/m² rzutu"
-        ),
-        correctIndex = 2
-    ),
-
-    Question(
-        text = "Butle z gazem propan–butan można stosować w budynkach o wysokości do:",
-        answers = listOf(
-            "15 m",
-            "25 m",
-            "55 m"
-        ),
-        correctIndex = 0
-    ),
-
-    Question(
+        id = 473,
         text = "W samochodzie gaśniczym GBA 2,5/16 zgodnie z regulaminem rozwinięcia znajdują się:",
         answers = listOf(
             "dwie wytwornice piany średniej WP-2-75 i jedna wytwornica WP-2-150",
@@ -115,16 +77,14 @@ val questionsPart20 = listOf(
     ),
 
     Question(
+        id = 474,
         text = "Sekcja GCBA 6/32 „Jelcz” przy podaniu jednego prądu wody może rozwinąć się na maksymalną odległość:",
-        answers = listOf(
-            "560 m",
-            "360 m",
-            "400 m"
-        ),
+        answers = listOf("560 m", "360 m", "400 m"),
         correctIndex = 1
     ),
 
     Question(
+        id = 475,
         text = "Aby mogła powstać mechaniczna piana gaśnicza niezbędne są:",
         answers = listOf(
             "woda, środek pianotwórczy, urządzenie",
@@ -135,36 +95,28 @@ val questionsPart20 = listOf(
     ),
 
     Question(
+        id = 476,
         text = "Zawór z zamknięciem grzybkowym i zamkiem rozpadającym się pod wpływem ciepła oraz rozpryskiwaczem nosi nazwę:",
-        answers = listOf(
-            "zraszacz",
-            "tryskacz",
-            "zawór alarmowo-gaśniczy"
-        ),
+        answers = listOf("zraszacz", "tryskacz", "zawór alarmowo-gaśniczy"),
         correctIndex = 1
     ),
 
     Question(
+        id = 477,
         text = "Zjawisko inhibicji (antykatalizy) procesu spalania występuje podczas gaszenia pożarów:",
-        answers = listOf(
-            "halonami",
-            "parą wodną",
-            "dwutlenkiem węgla"
-        ),
+        answers = listOf("halonami", "parą wodną", "dwutlenkiem węgla"),
         correctIndex = 0
     ),
 
     Question(
+        id = 478,
         text = "Planowany jest przewóz 100 kg dynamitu do kopalni. Na ile dni wcześniej należy zgłosić ten fakt do Komendanta Wojewódzkiego PSP?",
-        answers = listOf(
-            "14 dni",
-            "5 dni",
-            "2 dni"
-        ),
+        answers = listOf("14 dni", "5 dni", "2 dni"),
         correctIndex = 1
     ),
 
     Question(
+        id = 479,
         text = "Litera „X” w górnym rzędzie znaków na tablicy ostrzegawczej ADR oznacza:",
         answers = listOf(
             "uwaga, promieniowanie",
@@ -175,6 +127,7 @@ val questionsPart20 = listOf(
     ),
 
     Question(
+        id = 480,
         text = "Górne cyfry na tablicy ostrzegawczej ADR oznaczają:",
         answers = listOf(
             "numer identyfikacyjny przewożonej substancji",
@@ -185,36 +138,28 @@ val questionsPart20 = listOf(
     ),
 
     Question(
+        id = 481,
         text = "Pęcherzyk piany wytworzonej przez gaśnicę o symbolu GWP 9Y wypełnia:",
-        answers = listOf(
-            "CO₂",
-            "powietrze",
-            "azot"
-        ),
+        answers = listOf("CO₂", "powietrze", "azot"),
         correctIndex = 0
     ),
 
     Question(
+        id = 482,
         text = "APL to:",
-        answers = listOf(
-            "agregat piany lekkiej",
-            "agregat proszkowy lekki",
-            "agregat proszkowy specjalny"
-        ),
+        answers = listOf("agregat piany lekkiej", "agregat proszkowy lekki", "agregat proszkowy specjalny"),
         correctIndex = 0
     ),
 
     Question(
+        id = 483,
         text = "Ile nasad odprowadzających (wylotowych) Ø75 posiada rozdzielacz?",
-        answers = listOf(
-            "1",
-            "2",
-            "3"
-        ),
+        answers = listOf("1", "2", "3"),
         correctIndex = 0
     ),
 
     Question(
+        id = 484,
         text = "Symbol PP 2-12 oznacza prądownicę:",
         answers = listOf(
             "wodną o wydajności 20 l/min",
