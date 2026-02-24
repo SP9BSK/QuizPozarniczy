9a8f3a19fdf186b1032336ccccf9c1cb6bd2462b
SP9BSK <ratownikxl@gmail.com>
2026-02-24 09:45:51 +0100

    Update QuestionsPart1.kt

--- app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart1.kt
+++ app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart1.kt
@@ -5,30 +5,35 @@ import com.example.quizpozarniczy.model.Question
 val questionsPart1 = listOf(
 
     Question(
+        id = 101,
         text = "Pojemność zbiornika na środek pianotwórczy w samochodzie GBA 2,5/16 TYP 005 wynosi:",
         answers = listOf("160 l", "250 l", "500 l"),
         correctIndex = 1
     ),
 
     Question(
+        id = 102,
         text = "Najwyższą władzą w OSP jest:",
         answers = listOf("Prezes", "Zarząd", "Walne zebranie"),
         correctIndex = 2
     ),
 
     Question(
+        id = 103,
         text = "Gdzie w Polsce znajdują się relikwie św. Floriana?",
         answers = listOf("W Gnieźnie", "W Krakowie", "W Częstochowie"),
         correctIndex = 1
     ),
 
     Question(
+        id = 104,
         text = "Jaki gaz o właściwościach wybuchowych wydziela się podczas ładowania akumulatorów kwasowych?",
         answers = listOf("Metan", "Etan", "Wodór"),
         correctIndex = 2
     ),
 
     Question(
+        id = 105,
         text = "Oświetlenie awaryjne obejmuje:",
         answers = listOf(
             "Oświetlenie bezpieczeństwa i ewakuacyjne",
@@ -39,6 +44,7 @@ val questionsPart1 = listOf(
     ),
 
     Question(
+        id = 106,
         text = "Komendanta Głównego PSP powołuje i odwołuje:",
         answers = listOf(
             "Minister Spraw Wewnętrznych i Administracji",
@@ -49,12 +55,14 @@ val questionsPart1 = listOf(
     ),
 
     Question(
+        id = 107,
         text = "Gdzie sprawdzamy tętno u dorosłych poszkodowanych?",
         answers = listOf("Na przegubie ręki", "Na tętnicach szyjnych", "W pachwinach"),
         correctIndex = 1
     ),
 
     Question(
+        id = 108,
         text = "Projekt budowlany wymaga uzgodnienia z:",
         answers = listOf(
             "Rzeczoznawcą ds. zabezpieczeń przeciwpożarowych",
@@ -65,12 +73,14 @@ val questionsPart1 = listOf(
     ),
 
     Question(
+        id = 109,
         text = "Klasa odporności pożarowej dotyczy:",
         answers = listOf("Elementów budynku", "Budynku", "Oddzieleń przeciwpożarowych budynku"),
         correctIndex = 1
     ),
 
     Question(
+        id = 110,
         text = "Decyzję pokontrolną nakazującą usunięcie nieprawidłowości wydaje:",
         answers = listOf(
             "Komendant Miejski / Powiatowy PSP",
@@ -81,36 +91,42 @@ val questionsPart1 = listOf(
     ),
 
     Question(
+        id = 111,
         text = "Odległość hydrantu zewnętrznego od ścian budynku powinna wynosić:",
         answers = listOf("2–10 m", "Najmniej 5 m", "Do 5 m"),
         correctIndex = 1
     ),
 
     Question(
+        id = 112,
         text = "Produkowane obecnie gaśnice śniegowe posiadają zawór:",
         answers = listOf("Zbijakowy", "Pokrętny", "Szybkootwieralny"),
         correctIndex = 2
     ),
 
     Question(
+        id = 113,
         text = "W jakiej minimalnej odległości od lasu można rozpalić ognisko?",
         answers = listOf("70 m", "50 m", "100 m"),
         correctIndex = 2
     ),
 
     Question(
+        id = 114,
         text = "Czy samochód GBAM – 2/8 + 8 posiada motopompę?",
         answers = listOf("Może posiadać", "Nie", "Tak"),
         correctIndex = 2
     ),
 
     Question(
+        id = 115,
         text = "Minimalna odległość między butlą z gazem propan-butan a urządzeniem grzewczym powinna wynosić:",
         answers = listOf("2 m", "1,5 m", "0,5 m"),
         correctIndex = 1
     ),
 
     Question(
+        id = 116,
         text = "Gaz propan-butan jest:",
         answers = listOf(
             "Cięższy od powietrza",
@@ -121,6 +137,7 @@ val questionsPart1 = listOf(
     ),
 
     Question(
+        id = 117,
         text = "Co oznacza skrót KCKR?",
         answers = listOf(
             "Krajowe Centrum Koordynacji Ratownictwa",
@@ -131,6 +148,7 @@ val questionsPart1 = listOf(
     ),
 
     Question(
+        id = 118,
         text = "Samochód gaśniczy GBA 2,5/16 typ 005 posiada:",
         answers = listOf(
             "Urządzenie szybkiego natarcia W-52",
@@ -141,15 +159,16 @@ val questionsPart1 = listOf(
     ),
 
     Question(
+        id = 119,
         text = "Wydobywający się CO₂ z gaśnicy śniegowej osiąga temperaturę:",
         answers = listOf("2°C", "-10°C", "-79°C"),
         correctIndex = 2
     ),
 
     Question(
+        id = 120,
         text = "Front działania prądu gaśniczego w obronie wynosi:",
         answers = listOf("10 m", "20 m", "5 m"),
         correctIndex = 1
     )
-
 )
