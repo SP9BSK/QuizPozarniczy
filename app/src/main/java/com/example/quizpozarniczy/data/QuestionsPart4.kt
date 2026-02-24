1d03eaefcdf614669b076595a36fee8279704740
SP9BSK <ratownikxl@gmail.com>
2026-02-24 09:51:49 +0100

    Update QuestionsPart4.kt

--- app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart4.kt
+++ app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart4.kt
@@ -5,6 +5,7 @@ import com.example.quizpozarniczy.model.Question
 val questionsPart4 = listOf(
 
     Question(
+        id = 160,
         text = "W czasie akcji gaśniczej znak sygnalizacyjny wykonany przez strażaka prawą ręką odchyloną w bok w dół oznacza:",
         answers = listOf(
             "woda naprzód",
@@ -15,6 +16,7 @@ val questionsPart4 = listOf(
     ),
 
     Question(
+        id = 161,
         text = "Lekka woda to:",
         answers = listOf(
             "środek niezbędny do wytwarzania piany lekkiej",
@@ -25,6 +27,7 @@ val questionsPart4 = listOf(
     ),
 
     Question(
+        id = 162,
         text = "Hydrant wewnętrzny 52 posiada wydajność nominalną:",
         answers = listOf(
             "1,0 dm³/s",
@@ -35,6 +38,7 @@ val questionsPart4 = listOf(
     ),
 
     Question(
+        id = 163,
         text = "Z przewodów dymowych i spalinowych w budynkach opalanych paliwem płynnym należy usuwać zanieczyszczenia:",
         answers = listOf(
             "co najmniej 2 razy w roku",
@@ -45,6 +49,7 @@ val questionsPart4 = listOf(
     ),
 
     Question(
+        id = 164,
         text = "Resuscytacja:",
         answers = listOf(
             "śmierć kliniczna",
@@ -55,6 +60,7 @@ val questionsPart4 = listOf(
     ),
 
     Question(
+        id = 165,
         text = "Stacja paliw może być zlokalizowana od lasu w odległości:",
         answers = listOf(
             "50 m",
@@ -65,6 +71,7 @@ val questionsPart4 = listOf(
     ),
 
     Question(
+        id = 166,
         text = "Ubranie ochronne typu ciężkiego chroni przed temperaturą:",
         answers = listOf(
             "200°C",
@@ -75,6 +82,7 @@ val questionsPart4 = listOf(
     ),
 
     Question(
+        id = 167,
         text = "Kromos to:",
         answers = listOf(
             "środek gaśniczy",
@@ -85,6 +93,7 @@ val questionsPart4 = listOf(
     ),
 
     Question(
+        id = 168,
         text = "Ile butli z gazem płynnym o zawartości do 11 kg może być stosowanych jednocześnie w mieszkaniu?",
         answers = listOf(
             "1",
@@ -95,6 +104,7 @@ val questionsPart4 = listOf(
     ),
 
     Question(
+        id = 169,
         text = "Do gazów gaśniczych nie zalicza się:",
         answers = listOf(
             "etan",
@@ -105,6 +115,7 @@ val questionsPart4 = listOf(
     ),
 
     Question(
+        id = 170,
         text = "Co to jest strefa pożarowa?",
         answers = listOf(
             "strefa objęta pożarem",
@@ -115,6 +126,7 @@ val questionsPart4 = listOf(
     ),
 
     Question(
+        id = 171,
         text = "W przeciwpożarowy wyłącznik prądu należy wyposażać budynki:",
         answers = listOf(
             "użyteczności publicznej",
@@ -125,6 +137,7 @@ val questionsPart4 = listOf(
     ),
 
     Question(
+        id = 172,
         text = "Eksplozymetr służy do:",
         answers = listOf(
             "pomiaru stężeń gazu i par cieczy w powietrzu",
@@ -135,6 +148,7 @@ val questionsPart4 = listOf(
     ),
 
     Question(
+        id = 173,
         text = "Minimalna głębokość przeciwpożarowego zbiornika wodnego w miejscu czerpania powinna wynosić:",
         answers = listOf(
             "0,5 m",
@@ -145,6 +159,7 @@ val questionsPart4 = listOf(
     ),
 
     Question(
+        id = 174,
         text = "Podaj obowiązującą jednostkę miary odporności ogniowej elementów budowlanych:",
         answers = listOf(
             "godziny",
@@ -155,6 +170,7 @@ val questionsPart4 = listOf(
     ),
 
     Question(
+        id = 175,
         text = "Drzwi powinny otwierać się na zewnątrz pomieszczeń, w których może przebywać jednocześnie:",
         answers = listOf(
             "50 osób",
@@ -165,6 +181,7 @@ val questionsPart4 = listOf(
     ),
 
     Question(
+        id = 176,
         text = "Jak przed wojną nazywano kobiety działające w szeregach OSP?",
         answers = listOf(
             "ochotniczki",
@@ -175,6 +192,7 @@ val questionsPart4 = listOf(
     ),
 
     Question(
+        id = 177,
         text = "Co oznacza skrót NRO?",
         answers = listOf(
             "element budynku najlepiej rozprzestrzeniający ogień",
@@ -183,5 +201,4 @@ val questionsPart4 = listOf(
         ),
         correctIndex = 1
     )
-
 )
