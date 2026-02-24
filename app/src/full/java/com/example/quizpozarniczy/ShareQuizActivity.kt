850dffb696a5a36f8a8ea5c3b1d765db2366f51f
SP9BSK <ratownikxl@gmail.com>
2026-02-24 16:24:36 +0100

    Create ShareQuizActivity.kt

---
+++ app/src/full/java/com/example/quizpozarniczy/ShareQuizActivity.kt
@@ -0,0 +1,31 @@
+package com.example.quizpozarniczy
+
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.qrcode.QRCodeWriter
+
+class ShareQuizActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_share_quiz)
+
+        val qrData = intent.getStringExtra("QR_DATA") ?: return
+        val imageView = findViewById<ImageView>(R.id.imgQr)
+
+        val writer = QRCodeWriter()
+        val bitMatrix = writer.encode(qrData, BarcodeFormat.QR_CODE, 800, 800)
+
+        val bmp = Bitmap.createBitmap(800, 800, Bitmap.Config.RGB_565)
+        for (x in 0 until 800) {
+            for (y in 0 until 800) {
+                bmp.setPixel(x, y, if (bitMatrix[x, y]) 0xFF000000.toInt() else 0xFFFFFFFF.toInt())
+            }
+        }
+
+        imageView.setImageBitmap(bmp)
+    }
+}
