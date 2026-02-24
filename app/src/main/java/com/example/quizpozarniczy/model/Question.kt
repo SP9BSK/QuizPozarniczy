c1f1541486394d15ffe14a0c92d6a32fd5671087
SP9BSK <ratownikxl@gmail.com>
2026-02-24 10:27:41 +0100

    Update Question.kt

--- app/src/main/java/com/example/quizpozarniczy/model/Question.kt
+++ app/src/main/java/com/example/quizpozarniczy/model/Question.kt
@@ -1,7 +1,7 @@
 package com.example.quizpozarniczy.model
 
 data class Question(
-    val id: Int,
+   val id: Int = 0
     val text: String,
     val answers: List<String>,
     val correctIndex: Int,
