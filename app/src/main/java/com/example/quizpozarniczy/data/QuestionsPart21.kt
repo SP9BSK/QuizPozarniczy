0c375aa949d6ecbe96eeb62402679fe858e83377
SP9BSK <ratownikxl@gmail.com>
2026-02-24 10:18:39 +0100

    Update QuestionsPart21.kt

--- app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart21.kt
+++ app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart21.kt
@@ -5,6 +5,7 @@ import com.example.quizpozarniczy.model.Question
 val questionsPart21 = listOf(
 
     Question(
+        id = 486,
         text = "Z materiałów palnych może być wykonany garaż:",
         answers = listOf(
             "boksowy dla jednego samochodu osobowego",
@@ -15,6 +16,7 @@ val questionsPart21 = listOf(
     ),
 
     Question(
+        id = 487,
         text = "Symbol Z10 oznacza strefę zagrożenia wybuchem, w której:",
         answers = listOf(
             "mieszanina wybuchowa pyłów występuje często lub długotrwale w normalnych warunkach pracy",
@@ -25,36 +27,28 @@ val questionsPart21 = listOf(
     ),
 
     Question(
+        id = 488,
         text = "Czy klapy dymowe w dachach i stropach mogą być wykonane z materiałów palnych?",
-        answers = listOf(
-            "tak – do powierzchni 3,5 m²",
-            "nie",
-            "tak"
-        ),
+        answers = listOf("tak – do powierzchni 3,5 m²", "nie", "tak"),
         correctIndex = 2
     ),
 
     Question(
+        id = 489,
         text = "W strefach pożarowych o obciążeniu ogniowym 500 MJ/m² i wyższym jedna jednostka sprzętu gaśniczego 2 kg lub 2 dm³ powinna przypadać na każde:",
-        answers = listOf(
-            "150 m²",
-            "300 m²",
-            "nie stosuje się gaśnic o takich wielkościach"
-        ),
+        answers = listOf("150 m²", "300 m²", "nie stosuje się gaśnic o takich wielkościach"),
         correctIndex = 1
     ),
 
     Question(
+        id = 490,
         text = "W urządzenia przeciwpaniczne należy wyposażać drzwi pomieszczeń, w których może przebywać jednocześnie:",
-        answers = listOf(
-            "więcej niż 200 osób",
-            "więcej niż 300 osób",
-            "więcej niż 150 dzieci"
-        ),
+        answers = listOf("więcej niż 200 osób", "więcej niż 300 osób", "więcej niż 150 dzieci"),
         correctIndex = 1
     ),
 
     Question(
+        id = 491,
         text = "Przepisy dotyczące wymagań dla transportu drogowego towarów niebezpiecznych zawarte są w:",
         answers = listOf(
             "załącznikach A i B umowy ADR",
@@ -65,6 +59,7 @@ val questionsPart21 = listOf(
     ),
 
     Question(
+        id = 492,
         text = "Do czego służy trokomat?",
         answers = listOf(
             "do wytwarzania podciśnienia w linii ssawnej",
@@ -75,146 +70,49 @@ val questionsPart21 = listOf(
     ),
 
     Question(
-        text = "Co oznaczają tablice barwy pomarańczowej umieszczone na samochodzie?",
-        answers = listOf(
-            "uszkodzony samochód",
-            "samochód przewozi materiały niebezpieczne",
-            "samochód ciężarowy typu TIR"
-        ),
-        correctIndex = 1
-    ),
-
-    Question(
-        text = "Maksymalna, teoretyczna wysokość ssania wynosi:",
-        answers = listOf(
-            "5 m",
-            "7,5 m",
-            "10 m"
-        ),
-        correctIndex = 2
-    ),
-
-    Question(
-        text = "Co uzyskujemy w dyfuzorze?",
-        answers = listOf(
-            "zmniejszanie prędkości przepływu",
-            "zwiększanie prędkości przepływu",
-            "zmniejszanie wydatku"
-        ),
-        correctIndex = 0
-    ),
-
-    Question(
-        text = "Wydajność pistoletu proszkowego w samochodzie GPr-3000 wynosi:",
-        answers = listOf(
-            "5 kg/s",
-            "15 kg/s",
-            "30 kg/s"
-        ),
-        correctIndex = 0
-    ),
-
-    Question(
-        text = "Czy do budynku o wysokości 15 m, w którym mieści się dyskoteka na 45 osób, wymagane jest wykonanie drogi pożarowej?",
-        answers = listOf(
-            "nie",
-            "tak",
-            "tak, jeżeli budynek wykonany jest z materiałów palnych"
-        ),
-        correctIndex = 0
-    ),
-
-    Question(
-        text = "Ile wyjazdów należy wykonać na ogrodzony plac targowy o powierzchni 4 ha?",
-        answers = listOf(
-            "co najmniej 1",
-            "co najmniej 2",
-            "co najmniej 3"
-        ),
-        correctIndex = 2
-    ),
-
-    Question(
-        text = "Czy pomieszczenie chlewni, w której ludzie przebywają dłużej niż 4 godziny na dobę, uważa się za:",
-        answers = listOf(
-            "pomieszczenie przeznaczone na stały pobyt ludzi",
-            "pomieszczenie przeznaczone na czasowy pobyt ludzi",
-            "nie uważa się za pomieszczenie przeznaczone na pobyt ludzi"
-        ),
-        correctIndex = 1
-    ),
-
-    Question(
-        text = "W jakiej najmniejszej odległości od placu zabaw dziecięcych można zlokalizować parking dla 40 samochodów?",
-        answers = listOf(
-            "5 m",
-            "10 m",
-            "20 m"
-        ),
-        correctIndex = 1
-    ),
-
-    Question(
+        id = 493,
         text = "Jaka jest wymagana najmniejsza szerokość użytkowa biegu schodów wewnętrznych w budynku szkoły?",
-        answers = listOf(
-            "1,0 m",
-            "1,2 m",
-            "1,4 m"
-        ),
+        answers = listOf("1,0 m", "1,2 m", "1,4 m"),
         correctIndex = 1
     ),
 
     Question(
+        id = 494,
         text = "Czy do pomieszczeń technicznych na ostatniej kondygnacji budynku wysokiego może być doprowadzona instalacja gazowa?",
-        answers = listOf(
-            "nie",
-            "tak",
-            "tak, po zaopiniowaniu przez właściwą komendę wojewódzką PSP"
-        ),
+        answers = listOf("nie", "tak", "tak, po zaopiniowaniu przez właściwą komendę wojewódzką PSP"),
         correctIndex = 2
     ),
 
     Question(
+        id = 495,
         text = "W jakiej najmniejszej odległości od grzejnika CO można ustawić butlę z gazem propan–butan?",
-        answers = listOf(
-            "0,5 m",
-            "1,0 m",
-            "1,5 m"
-        ),
+        answers = listOf("0,5 m", "1,0 m", "1,5 m"),
         correctIndex = 0
     ),
 
     Question(
+        id = 496,
         text = "Czy w garażu oświetlonym wyłącznie światłem sztucznym wymagane jest oświetlenie awaryjne?",
-        answers = listOf(
-            "nie",
-            "tak",
-            "tak, jeżeli powierzchnia garażu przekracza 1000 m²"
-        ),
+        answers = listOf("nie", "tak", "tak, jeżeli powierzchnia garażu przekracza 1000 m²"),
         correctIndex = 2
     ),
 
     Question(
+        id = 497,
         text = "Czy w dachu, dla którego wymagana jest odporność ogniowa, można wykonać świetliki bez tej odporności?",
-        answers = listOf(
-            "nie można",
-            "można, jeżeli powierzchnia nie przekracza 100 m²",
-            "można, jeżeli powierzchnia świetlików nie przekracza 20% powierzchni dachu"
-        ),
+        answers = listOf("nie można", "można, jeżeli powierzchnia nie przekracza 100 m²", "można, jeżeli powierzchnia świetlików nie przekracza 20% powierzchni dachu"),
         correctIndex = 2
     ),
 
     Question(
+        id = 498,
         text = "O ile można zwiększyć długość przejścia ewakuacyjnego w garażu zamkniętym przy zastosowaniu stałych urządzeń gaśniczych?",
-        answers = listOf(
-            "o 50%",
-            "o 100%",
-            "nie można zwiększyć"
-        ),
+        answers = listOf("o 50%", "o 100%", "nie można zwiększyć"),
         correctIndex = 2
     ),
 
     Question(
+        id = 499,
         text = "W jakim przypadku nie normuje się odległości między budynkami?",
         answers = listOf(
             "jeżeli wykonane są z materiałów niepalnych",
@@ -225,6 +123,7 @@ val questionsPart21 = listOf(
     ),
 
     Question(
+        id = 500,
         text = "Ile kondygnacji może mieć budynek tymczasowy przeznaczony na pobyt ludzi?",
         answers = listOf(
             "musi być jednokondygnacyjny",
@@ -235,36 +134,28 @@ val questionsPart21 = listOf(
     ),
 
     Question(
+        id = 501,
         text = "Minimalna głębokość przeciwpożarowego zbiornika wodnego wynosi:",
-        answers = listOf(
-            "0,5 m",
-            "0,7 m",
-            "1,2 m"
-        ),
+        answers = listOf("0,5 m", "0,7 m", "1,2 m"),
         correctIndex = 0
     ),
 
     Question(
+        id = 502,
         text = "Czy dla stacji gazu płynnego zlokalizowanej poza granicą jednostki osadniczej należy zapewnić wodę do zewnętrznego gaszenia pożaru?",
-        answers = listOf(
-            "nie",
-            "tak",
-            "tak, jeżeli pojemność zbiorników nie przekracza 20 m³"
-        ),
+        answers = listOf("nie", "tak", "tak, jeżeli pojemność zbiorników nie przekracza 20 m³"),
         correctIndex = 1
     ),
 
     Question(
+        id = 503,
         text = "Jaka powinna być maksymalna odległość hydrantu zewnętrznego od krawędzi drogi?",
-        answers = listOf(
-            "2 m",
-            "5 m",
-            "15 m"
-        ),
+        answers = listOf("2 m", "5 m", "15 m"),
         correctIndex = 2
     ),
 
     Question(
+        id = 504,
         text = "Kiedy strażak może dokonać czynności kontrolno-rozpoznawczych ładunków niebezpiecznych przewożonych pojazdami?",
         answers = listOf(
             "nie ma prawa do kontroli",
