ea45b2c98b584933f354a25d32528322dfa3dc10
SP9BSK <ratownikxl@gmail.com>
2026-02-26 18:23:01 +0100

    Update ResultsActivity.kt

--- app/src/full/java/com/example/quizpozarniczy/ResultsActivity.kt
+++ app/src/full/java/com/example/quizpozarniczy/ResultsActivity.kt
@@ -1,11 +1,17 @@
 package com.example.quizpozarniczy
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ResultsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_results)
+
+        val tv = findViewById<TextView>(R.id.tvResults)
+        val text = ScanResultsStore.getAll().joinToString("\n")
+        tv.text = if (text.isEmpty()) "Brak wyników" else text
     }
 }
