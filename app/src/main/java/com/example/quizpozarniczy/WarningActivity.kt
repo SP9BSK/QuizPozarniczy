b81ed756b399374bcb9a946777b11857f0afb0f5
SP9BSK <ratownikxl@gmail.com>
2026-02-27 16:02:59 +0100

    Create WarningActivity.kt

---
+++ app/src/main/java/com/example/quizpozarniczy/WarningActivity.kt
@@ -0,0 +1,31 @@
+package com.example.quizpozarniczy
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class WarningActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_warning)
+
+        val prefs = getSharedPreferences("app_prefs", MODE_PRIVATE)
+
+        val tvWarning = findViewById<TextView>(R.id.tvWarning)
+        val btnNext = findViewById<Button>(R.id.btnNext)
+
+        tvWarning.text = "UWAGA!\nZapoznaj się z instrukcją i edytuj pytania lokalne."
+
+        btnNext.setOnClickListener {
+            prefs.edit()
+                .putBoolean("warning_shown", true)
+                .apply()
+
+            startActivity(Intent(this, StartActivity::class.java))
+            finish()
+        }
+    }
+}
