7913b22679b8c30df667bfa94801a15c0d6c1777
SP9BSK <ratownikxl@gmail.com>
2026-02-28 11:47:33 +0100

    Update SupportActivity.kt

--- app/src/main/java/com/example/quizpozarniczy/SupportActivity.kt
+++ app/src/main/java/com/example/quizpozarniczy/SupportActivity.kt
@@ -7,5 +7,7 @@ class SupportActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_support)
+        
+        window.addFlags(android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 }
