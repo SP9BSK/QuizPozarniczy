a28d5546a6a32cfa0c8addc2df1642292c40a4d6
SP9BSK <ratownikxl@gmail.com>
2026-03-06 11:23:50 +0100

    Create QuestionsPart26.kt

---
+++ app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart26.kt
@@ -0,0 +1,390 @@
+package com.example.quizpozarniczy.data
+
+import com.example.quizpozarniczy.model.Question
+
+val questionsPart26 = listOf(
+
+    Question(
+        id = 1101,
+        text = "Zespół startujący w konkurencji – ćwiczenia bojowe podczas Mistrzostw Polski w Sporcie Pożarniczym liczy:",
+        answers = listOf(
+            "4 zawodników",
+            "7 zawodników",
+            "8 zawodników"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        id = 1102,
+        text = "Energia cieplna, wyrażona w MJ, która może powstać przy spaleniu materiałów palnych znajdujących się w pomieszczeniu, strefie pożarowej lub składowisku materiałów stałych przypadająca na jednostkę powierzchni tego obiektu to:",
+        answers = listOf(
+            "obciążenie ogniowe",
+            "gęstość obciążenia ogniowego",
+            "wielkość pożaru"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        id = 1103,
+        text = "Względny czas trwania pożaru to:",
+        answers = listOf(
+            "czas, w którym ulegną spaleniu materiały palne znajdujące się w pomieszczeniu lub składowisku materiałów stałych w strefie pożarowej",
+            "czas, w którym ulegną spaleniu wszystkie materiały znajdujące się w pomieszczeniu lub strefie pożarowej",
+            "czas, w którym ulegną spaleniu tylko stałe materiały palne znajdujące się w pomieszczeniu"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        id = 1104,
+        text = "Wysoko umieszczone oświetlenie podstawowe i/lub ewakuacyjne powinno być usytuowane powyżej:",
+        answers = listOf(
+            "180 cm od podłogi",
+            "200 cm od podłogi",
+            "220 cm od podłogi"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        id = 1105,
+        text = "Pod pojęciem gotowości bojowej rozumie się:",
+        answers = listOf(
+            "pośpieszne osiągnięcie stanu gotowości bojowej podziału straży pożarnej",
+            "zdolność podziału straży pożarnej do podjęcia akcji ratowniczej w określonym czasie",
+            "czynności podziałów taktycznych straży pożarnych, mające na celu zlikwidowanie pożaru"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        id = 1106,
+        text = "Czy do budynku niskiego, w którym mieści się dyskoteka na 100 osób wymagane jest wykonanie drogi pożarowej:",
+        answers = listOf(
+            "nie",
+            "tak",
+            "tak, ale w przypadku gdy budynek jest z materiałów palnych"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        id = 1107,
+        text = "Za pomieszczenie przeznaczone na czasowy pobyt ludzi uważa się pomieszczenie, w którym przebywanie tych samych osób w ciągu doby trwa:",
+        answers = listOf(
+            "do 2 godzin",
+            "od 2 do 4 godzin włącznie",
+            "powyżej 4 godzin"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        id = 1108,
+        text = "Czy w budynku mieszkalnym, mającym instalację gazową zasilaną z sieci można stosować instalację gazową z butli:",
+        answers = listOf(
+            "nie",
+            "tak",
+            "tak, ale tylko w budynku niskim"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        id = 1109,
+        text = "Na którym terenie w 1945 r. najlepiej wśród autochtonów rozwijały się OSP:",
+        answers = listOf(
+            "Na Dolnym Śląsku",
+            "Na Ziemi Opolskiej",
+            "Na Warmii i Mazurach"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        id = 1110,
+        text = "Podczas zjazdu, przechodzenia po linie, przechodzenia węzła lub stanowiska należy bezwzględnie stosować zasadę:",
+        answers = listOf(
+            "jednego punktu wpięcia ratownika w linę",
+            "dwóch punktów wpięcia ratowników w linę",
+            "trzech punktów wpięcia ratowników w linę"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        id = 1111,
+        text = "Hydropult to:",
+        answers = listOf(
+            "urządzenie samoczynnie składające się z pompy i zasilanego przez nią zbiornika wodno-powietrznego",
+            "podręczny sprzęt gaśniczy, uruchamiany ręcznie. Środek gaśniczy wyrzucany jest ze zbiornika własnego urządzenia za pomocą ssąco-tłocznej pompy ręcznej",
+            "podręczny sprzęt gaśniczy służący do gaszenia pożarów w zarodku. Pompa ssąco-tłocząca bez własnego zbiornika"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        id = 1112,
+        text = "Czas nurkowania liczy się:",
+        answers = listOf(
+            "od początku przebywania w podwyższonym ciśnieniu, to jest od zanurzenia aż do momentu ponownego zrównania się ciśnienia wewnętrznego nurka z ciśnieniem atmosferycznym tj. wynurzenia",
+            "od początku do końca ćwiczeń lub akcji ratowniczej na akwenie wodnym",
+            "od początku przebywania w maksymalnie podwyższonym ciśnieniu, to jest od maksymalnej głębokości zanurzenia aż do momentu ponownego zrównania się ciśnienia wewnętrznego nurka z ciśnieniem atmosferycznym tj. wynurzenia"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        id = 1113,
+        text = "Geometryczna wysokość tłoczenia to:",
+        answers = listOf(
+            "rzeczywista pionowa odległość między osią nasady a punktem najwyższego rzutu wody, mierzona w metrach",
+            "suma geometrycznej wysokości tłoczenia, wyrażana w metrach",
+            "suma geometrycznej wysokości ssania, wyrażana w metrach"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        id = 1114,
+        text = "W 1853 r. pruskie władze wydały ordynację, która stworzyła podstawy prawne do:",
+        answers = listOf(
+            "Regulacji zakresu działania straży zawodowych",
+            "Regulacji zaopatrywania straży w sprzęt",
+            "Wydawania przez miasta własnych statutów dla straży"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        id = 1115,
+        text = "Przystępując do natarcia przy użyciu piany gaśniczej, przyjmuje się określony obliczeniowy czas podawania środka gaśniczego. W przypadku piany ciężkiej czas ten wynosi:",
+        answers = listOf("5 min", "8 min", "10 min"),
+        correctIndex = 0
+    ),
+
+    Question(
+        id = 1116,
+        text = "Krajowy system ratowniczo-gaśniczy jest wspomagany na poziomie powiatowym przez organizacje pozarządowe i humanitarne na zasadach:",
+        answers = listOf("obowiązku", "dobrowolności", "zgody pisemnej wojewody"),
+        correctIndex = 1
+    ),
+
+    Question(
+        id = 1117,
+        text = "Opracowanie i realizację wojewódzkiego planu ratowniczego koordynuje:",
+        answers = listOf(
+            "komendant wojewódzki PSP",
+            "komendant główny PSP",
+            "marszałek samorządu wojewódzkiego"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        id = 1118,
+        text = "Powiatowe i wojewódzkie plany ratownicze aktualizuje się co najmniej:",
+        answers = listOf("co roku i zawsze w razie potrzeby", "co 2 lata", "co 3 lata"),
+        correctIndex = 0
+    ),
+
+    Question(
+        id = 1119,
+        text = "Kierowanie działaniem ratowniczym prowadzone jest przez:",
+        answers = listOf(
+            "sztab, jeśli jest on ujęty w planie ratowniczym",
+            "dowódcę podmiotu ratowniczego, który pierwszy przybył na miejsce zdarzenia",
+            "oficera policji, szczególnie gdy jest kilka podmiotów ratowniczych na miejscu zdarzenia"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        id = 1120,
+        text = "Na terenie którego z zaborów powstała pierwsza ochotnicza straż pożarna?",
+        answers = listOf(
+            "w Galicji (zabór austriacki)",
+            "w zaborze pruskim",
+            "w Królestwie Polskim (zabór rosyjski)"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        id = 1121,
+        text = "W którym roku Sejm Ustawodawczy uchwalił pierwszą w Polsce Ludowej ustawę o ochronie przeciwpożarowej?",
+        answers = listOf("1947 r.", "1949 r.", "1950 r."),
+        correctIndex = 2
+    ),
+
+    Question(
+        id = 1122,
+        text = "W latach 1950–1954 komendy wojewódzkie i powiatowe straży podlegały ministerstwu:",
+        answers = listOf(
+            "Administracji Publicznej",
+            "Gospodarki Komunalnej",
+            "Spraw Wewnętrznych"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        id = 1123,
+        text = "W roku 1971 i 1973 powołano dwuletnie Szkoły Chorążych Pożarnictwa w:",
+        answers = listOf(
+            "Warszawie i Poznaniu",
+            "Poznaniu i Wrocławiu",
+            "Krakowie i Poznaniu"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        id = 1124,
+        text = "Od którego roku funkcjonuje krajowy system ratowniczo-gaśniczy?",
+        answers = listOf("1992 r.", "1994 r.", "1995 r."),
+        correctIndex = 2
+    ),
+
+    Question(
+        id = 1125,
+        text = "Zdolność cieczy palnych do wytworzenia par w ilości wystarczającej do zapalenia jest określona za pomocą temperatury:",
+        answers = listOf("zapłonu", "zapalenia", "wrzenia"),
+        correctIndex = 0
+    ),
+
+    Question(
+        id = 1126,
+        text = "Powierzchnia wszystkich lasów w Polsce wynosi ok. 9 mln hektarów, co odpowiada lesistości:",
+        answers = listOf("37 proc.", "28 proc.", "19 proc."),
+        correctIndex = 1
+    ),
+
+    Question(
+        id = 1127,
+        text = "Opracowanie i aktualizację powiatowego planu ratowniczego koordynuje:",
+        answers = listOf(
+            "komendant wojewódzki PSP",
+            "komendant powiatowy (miejski) PSP",
+            "rada powiatu"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        id = 1128,
+        text = "Wojewódzkie plany ratownicze zatwierdza:",
+        answers = listOf("komendant wojewódzki PSP", "wojewoda", "wojewódzki sejmik samorządowy"),
+        correctIndex = 1
+    ),
+
+    Question(
+        id = 1129,
+        text = "Batalion to:",
+        answers = listOf(
+            "związek pododdziałów i oddziałów przewidziany do prowadzenia działań ratowniczych na terenie całego województwa",
+            "oddział w sile od trzech do pięciu kompanii oraz dowódca",
+            "związek specjalistycznych grup ratowniczych, przewidzianych do przeprowadzania działań ratowniczych na terenie własnego województwa"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        id = 1130,
+        text = "Najmniejsza wysokość drzwi w świetle na drogach ewakuacyjnych powinna wynosić:",
+        answers = listOf("190 cm", "195 cm", "200 cm"),
+        correctIndex = 2
+    ),
+
+    Question(
+        id = 1131,
+        text = "Lekki dach to:",
+        answers = listOf(
+            "dach, z którego nie wolno prowadzić akcji gaśniczej",
+            "dach o ciężarze nie przekraczającym 75 kg/m² rzutu, licząc z konstrukcją nośną dachu",
+            "dach z materiałów niepalnych lub trudno zapalnych o ciężarze nie przekraczającym 75 kg/m² rzutu"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        id = 1132,
+        text = "Butle z gazem propan–butanem można stosować w budynkach o wysokości do:",
+        answers = listOf("15 m", "25 m", "55 m"),
+        correctIndex = 0
+    ),
+
+    Question(
+        id = 1133,
+        text = "Co oznaczają tablice barwy pomarańczowej umiejscowione na samochodzie?",
+        answers = listOf(
+            "uszkodzony samochód",
+            "samochód przewozi materiały niebezpieczne",
+            "samochód ciężarowy – TIR"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        id = 1134,
+        text = "Maksymalna i teoretyczna wysokość ssania wynosi:",
+        answers = listOf("5 m", "7,5 m", "10 m"),
+        correctIndex = 2
+    ),
+
+    Question(
+        id = 1135,
+        text = "Co uzyskujemy w dyfuzorze?",
+        answers = listOf(
+            "zmniejszanie prędkości przepływu",
+            "zwiększanie prędkości przepływu",
+            "zmniejszanie wydatku"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        id = 1136,
+        text = "Wydajność pistoletu proszkowego w samochodzie GPr–3000 wynosi:",
+        answers = listOf("5 kg/s", "15 kg/s", "30 kg/s"),
+        correctIndex = 0
+    ),
+
+    Question(
+        id = 1137,
+        text = "Czy do budynku o wysokości 15 m, w którym mieści się dyskoteka na 45 osób wymagane jest wykonanie drogi przeciwpożarowej:",
+        answers = listOf(
+            "nie",
+            "tak",
+            "tak, ale w przypadku gdy budynek wykonany jest z materiałów palnych"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        id = 1138,
+        text = "Ile wyjazdów należy wykonać na ogrodzony plac targowy o powierzchni 4 ha:",
+        answers = listOf("co najmniej 1", "co najmniej 2", "co najmniej 3"),
+        correctIndex = 2
+    ),
+
+    Question(
+        id = 1139,
+        text = "Pomieszczenie chlewni, w której zachodzi konieczność przebywania ludzi dłużej niż 4 godziny na dobę, uważa się za:",
+        answers = listOf(
+            "pomieszczenie przeznaczone na stały pobyt ludzi",
+            "pomieszczenie przeznaczone na czasowy pobyt ludzi",
+            "nie uważa się za pomieszczenie przeznaczone na pobyt ludzi"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        id = 1140,
+        text = "W jakiej najmniejszej odległości od placu zabaw dziecięcych można zlokalizować miejsce parkingowe dla 40 samochodów?",
+        answers = listOf("5 m", "10 m", "20 m"),
+        correctIndex = 1
+    )
+)
