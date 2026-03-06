c46263376c2cb44c746180446764f86922e42a23
SP9BSK <ratownikxl@gmail.com>
2026-03-06 10:52:48 +0100

    Create QuestionsPart25.kt

---
+++ app/src/main/java/com/example/quizpozarniczy/data/QuestionsPart25.kt
@@ -0,0 +1,154 @@
+package com.example.quizpozarniczy.data
+
+import com.example.quizpozarniczy.model.Question
+
+val questionsPart25 = listOf(
+
+    Question(
+        id = 1001,
+        text = "Klapa dymowa to:",
+        answers = listOf(
+            "urządzenie służące do gaszenia cieczy palnych w zbiornikach",
+            "urządzenie służące do zamykania zbiorników z gazami palnymi",
+            "pokrywa zamontowana w dachu budynku umożliwiająca usunięcie z pomieszczeń dymu i gorących gazów pożarowych"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        id = 1002,
+        text = "Dojście ewakuacyjne to:",
+        answers = listOf(
+            "długość drogi ewakuacyjnej od wyjścia z pomieszczenia na tę drogę do wyjścia do innej strefy pożarowej lub na zewnątrz budynku, którą mierzy się wzdłuż osi drogi ewakuacyjnej",
+            "odległość od najdalszego miejsca w pomieszczeniu, w którym może przebywać człowiek do wyjścia na drogę ewakuacyjną",
+            "odległość od najdalszego miejsca w pomieszczeniu, w którym może przebywać człowiek do wyjścia na zewnątrz budynku"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        id = 1003,
+        text = "Ściany oddzielające pomieszczenie zagrożone wybuchem od innych pomieszczeń powinny być odporne na parcie o wartości:",
+        answers = listOf("25 kPa", "15 kPa", "35 kPa"),
+        correctIndex = 1
+    ),
+
+    Question(
+        id = 1004,
+        text = "Podaj sposób reanimacji prowadzonej przez 1 ratownika:",
+        answers = listOf("15 na 2", "10 na 2", "30 na 2"),
+        correctIndex = 2
+    ),
+
+    Question(
+        id = 1005,
+        text = "Poddawanie butli z gazami palnymi bezpośrednio działaniu ognia jest:",
+        answers = listOf(
+            "zabronione w obiektach niezagrożonych wybuchem",
+            "dozwolone w przypadku zachowania wszelkich możliwych środków ostrożności i zasad BHP",
+            "zabronione"
+        ),
+        correctIndex = 2
+    ),
+
+    Question(
+        id = 1006,
+        text = "Odporność ogniowa to cecha elementu budowlanego mierzona:",
+        answers = listOf("w stopniach C", "w kg/m³", "w minutach"),
+        correctIndex = 2
+    ),
+
+    Question(
+        id = 1007,
+        text = "Jaki gaz o właściwościach wybuchowych wydziela się podczas ładowania akumulatorów zasadowych?",
+        answers = listOf("metan", "wodór", "nie wydziela się żaden gaz"),
+        correctIndex = 2
+    ),
+
+    Question(
+        id = 1008,
+        text = "W którym roku Centralne Muzeum Pożarnictwa w Mysłowicach otrzymało swój statut?",
+        answers = listOf(
+            "w styczniu 1982 roku",
+            "w lutym 1993 roku",
+            "w sierpniu 1975 roku"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        id = 1009,
+        text = "Geometryczna wysokość podnoszenia to:",
+        answers = listOf(
+            "suma geometrycznej wysokości ssania i tłoczenia, wyrażona w metrach",
+            "suma geometrycznej wysokości ssania i tłoczenia wyrażonej w litrach",
+            "suma geometrycznej wysokości ssania wyrażona w metrach"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        id = 1010,
+        text = "II stopień oparzenia to:",
+        answers = listOf(
+            "oparzenie, którego objawem jest zaczerwienienie skóry, obrzęk i uczucie pieczenia",
+            "oparzenie, którego objawem jest pojawienie się na zaczerwienionej i obrzękniętej skórze pęcherzy z żółtawym płynem surowiczym, towarzyszy temu ostry ból",
+            "oparzenie, które cechuje się martwicą całej grubości skóry oraz uszkodzeniem tkanek położonych głębiej"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        id = 1011,
+        text = "Główny Sąd Honorowy OSP RP składa się z:",
+        answers = listOf(
+            "od 1 do 6 członków",
+            "od 7 do 11 członków",
+            "od 12 do 16 członków"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        id = 1012,
+        text = "Jaki wzór chemiczny ma fenol?",
+        answers = listOf("C6H5OH", "CH2O", "CH3Cl"),
+        correctIndex = 0
+    ),
+
+    Question(
+        id = 1013,
+        text = "Budynki wysokościowe to budynki o wysokości:",
+        answers = listOf("powyżej 55 m", "powyżej 75 m", "powyżej 25 m"),
+        correctIndex = 0
+    ),
+
+    Question(
+        id = 1014,
+        text = "Do samochodów pożarniczych średnich zalicza się samochody o masie całkowitej:",
+        answers = listOf(
+            "od 3,5 do 12 ton",
+            "od 2 do 3,5 ton",
+            "od 12 do 15 ton"
+        ),
+        correctIndex = 0
+    ),
+
+    Question(
+        id = 1015,
+        text = "Co uzyskujemy w konfuzorze?",
+        answers = listOf(
+            "zmniejszenie prędkości przepływu",
+            "zwiększenie prędkości przepływu",
+            "zmniejszenie wydatku"
+        ),
+        correctIndex = 1
+    ),
+
+    Question(
+        id = 1016,
+        text = "Na terenie jakiego województwa zorganizowano w 1945 r. pierwszy w Polsce Ludowej „Tydzień Obrony Przeciwpożarowej”?",
+        answers = listOf("warszawskiego", "krakowskiego", "lubuskiego"),
+        correctIndex = 2
+    )
+)
